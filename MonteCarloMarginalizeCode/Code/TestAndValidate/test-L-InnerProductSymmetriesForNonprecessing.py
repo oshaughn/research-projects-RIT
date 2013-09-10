441d404e234e947cd3846c39fd1c51db73132bdb
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2013-09-10 17:18:45 -0500

    Updating

---
+++ MonteCarloMarginalizeCode/Code/TestAndValidate/test-L-InnerProductSymmetriesForNonprecessing.py
@@ -0,0 +1,24 @@
+from  lalsimutils import *
+from marginalized_likelihood import *
+
+P = ChooseWaveformParams()
+
+psd=lalsim.SimNoisePSDiLIGOModel
+
+Lmax = 5
+
+fSample = 4096.
+
+
+hlms = lalsim.SimInspiralChooseTDModes(P.phiref, P.deltaT, P.m1, P.m2,
+                                      # P.s1x, P.s1y, P.s1z, P.s2x, P.s2y, P.s2z, 
+                                       P.fmin, P.fref, P.dist,
+                                      # P.incl, 
+                                       P.lambda1, P.lambda2, P.waveFlags, P.nonGRparams,
+                                       P.ampO, P.phaseO, Lmax, P.approx)
+
+
+crossTerms = ComputeModeCrossTermIP(hlms, psd, P.fmin, fSample/2.)
+
+print crossTerms
+
