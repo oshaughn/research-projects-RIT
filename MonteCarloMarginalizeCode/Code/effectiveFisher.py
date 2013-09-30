7a2861cef41deda57f1eee0bd360b922f6193459
Evan Ochsner <evan.ochsner@ligo.org>
2013-09-30 01:08:45 -0500

    Added effective Fisher matrix module and test code

---
+++ MonteCarloMarginalizeCode/Code/effectiveFisher.py
@@ -0,0 +1,254 @@
+# Copyright (C) 2013  Evan Ochsner
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+Module of routines to compute an effective Fisher matrix and related utilities,
+such as finding a region of interest and laying out a grid over it
+"""
+
+from lalsimutils import *
+from scipy.optimize import leastsq
+
+__author__ = "Evan Ochsner <evano@gravity.phys.uwm.edu>"
+
+
+def effectiveFisher(residual_func, *flat_grids):
+    """
+    DOCUMENT ME!!!
+    """
+    x0 = np.ones(len(flat_grids))
+    fitgamma = leastsq(residual_func, x0=x0, args=tuple(flat_grids))
+    return fitgamma[0]
+
+def evaluate_ip_on_grid(hfSIG, P, IP, param_names, grid):
+    """
+    Evaluate IP.ip(hsig, htmplt) everywhere on a multidimensional grid
+    """
+    Nparams = len(param_names)
+    Npts = len(grid)
+    assert len(grid[0])==Nparams
+    return [update_params_ip(hfSIG, P, IP, param_names, grid[i])
+            for i in xrange(Npts)]
+
+
+def update_params_ip(hfSIG, P, IP, param_names, vals):
+    """
+    Update the values of 1 or more member of P, recompute norm_hoff(P),
+    and return IP.ip(hfSIG, norm_hoff(P))
+
+    Inputs:
+        - hfSIG: A COMPLEX16FrequencySeries of a fixed, unchanging signal
+        - P: A ChooseWaveformParams object describing a varying template
+        - IP: An InnerProduct object
+        - param_names: An array of strings of parameters to be updated.
+            e.g. [ 'm1', 'm2', 'incl' ]
+        - vals: update P to have these parameter values. Must have as many
+            vals as length of param_names, ordered the same way
+
+    Outputs:
+        - A COMPLEX16FrequencySeries, same as norm_hoff(P, IP)
+    """
+    hfTMPLT = update_params_norm_hoff(P, IP, param_names, vals)
+    if IP.full_output == True:
+        rho, rhoSeries, rhoIdx, rhoArg = IP.ip(hfSIG, hfTMPLT)
+        return rho
+    else:
+        return IP.ip(hfSIG, hfTMPLT)
+
+
+def update_params_norm_hoff(P, IP, param_names, vals, verbose=False):
+    """
+    Update the values of 1 or more member of P and recompute norm_hoff(P).
+
+    Inputs:
+        - P: A ChooseWaveformParams object
+        - IP: An InnerProduct object
+        - param_names: An array of strings of parameters to be updated.
+            e.g. [ 'm1', 'm2', 'incl' ]
+        - vals: update P to have these parameter values. Must have as many
+            vals as length of param_names, ordered the same way
+
+    Outputs:
+        - A COMPLEX16FrequencySeries, same as norm_hoff(P, IP)
+    """
+    special_params = []
+    special_vals = []
+    assert len(param_names)==len(vals)
+    for i, val in enumerate(vals):
+        if hasattr(P, param_names[i]): # Update an attribute of P...
+            setattr(P, param_names[i], val)
+        else: # Either an incorrect param name, or a special case...
+            special_params.append(param_names[i])
+            special_vals.append(val)
+
+    # Check allowed special cases of params not in P, e.g. Mc and eta
+    if special_params==['Mc','eta']:
+        m1, m2 = m1m2(special_vals[0], special_vals[1]) # m1,m2 = m1m2(Mc,eta)
+        setattr(P, 'm1', m1)
+        setattr(P, 'm2', m2)
+    elif special_params==['eta','Mc']:
+        m1, m2 = m1m2(special_vals[1], special_vals[0])
+        setattr(P, 'm1', m1)
+        setattr(P, 'm2', m2)
+    elif special_params != []:
+        print special_params
+        raise Exception
+
+    if verbose==True: # for debugging - make sure params change properly
+        P.print_params()
+    return norm_hoff(P, IP)
+
+
+
+def find_effective_Fisher_region(P, IP, target_match, param_names,
+        max_param_width):
+    """
+    Arguments:
+        - P: a ChooseWaveformParams object describing a target signal
+        - IP: An inner product class to compute overlaps.
+                Should have deltaF and length consistent with P
+        - target_match: find parameter variation where overlap is target_match.
+                Should be a real number in [0,1]
+        - param_names: array of string names for members of P to vary
+                e.g. ['Mc', 'eta']
+        - max_param_width: array of max variations of each param in param_names
+    
+    Returns:
+        Array of boundaries of a hypercube meant to encompass region where
+                match is >= target_match.
+                e.g. [ [3.12,3.16] , [0.12, 0.18] ]
+
+    N.B. Only checks variations along parameter axes. If params are correlated,
+    may get better matches off-axis, and the hypercube won't fully encompass
+    region where target_match is achieved. Therefore, allow a generous
+    safety factor in your value of 'target_match'.
+    """
+    # FIXME: Use a root-finder to bound a region of interest
+    Nparams = len(param_names)
+    assert len(max_param_width) == Nparams
+    param_cube = np.zeros( (Nparams, 2) )
+    PS = P.copy()
+    df = findDeltaF(PS)
+    PS.deltaF=df
+    PT = PS.copy()
+
+    return param_cube
+
+#
+# Routines to make various types of grids for arbitrary dimension
+#
+def make_regular_1d_grids(param_ranges, pts_per_dim):
+    """
+    Inputs: 
+        - param_ranges is an array of parameter bounds, e.g.:
+        [ [p1_min, p1_max], [p2_min, p2_max], ..., [pN_min, pN_max] ]
+        - pts_per_dim is either:
+            a) an integer - use that many pts for every parameter
+            b) an array of integers of same length as param_ranges, e.g.
+                [ N1, N2, ..., NN ]
+                the n-th entry is the number of pts for the n-th parameter
+
+    Outputs:
+        outputs N separate 1d arrays of evenly spaced values of that parameter,
+        where N = len(param_ranges)
+    """
+    Nparams = len(param_ranges)
+    assert len(pts_per_dim)
+    grid1d = []
+    for i in range(Nparams):
+        MIN = param_ranges[i][0]
+        MAX = param_ranges[i][1]
+        STEP = (MAX-MIN)/(pts_per_dim[i]-1)
+        EPS = STEP/100.
+        grid1d.append( np.arange(MIN,MAX+EPS,STEP) )
+
+    return tuple(grid1d)
+
+def multi_dim_meshgrid(*arrs):
+    """
+    Version of np.meshgrid generalized to arbitrary number of dimensions.
+    Taken from: http://stackoverflow.com/questions/1827489/numpy-meshgrid-in-3d
+    """
+    arrs = tuple(reversed(arrs))
+    lens = map(len, arrs)
+    dim = len(arrs)
+
+    sz = 1
+    for s in lens:
+        sz*=s
+
+    ans = []    
+    for i, arr in enumerate(arrs):
+        slc = [1]*dim
+        slc[i] = lens[i]
+        arr2 = np.asarray(arr).reshape(slc)
+        for j, sz in enumerate(lens):
+            if j!=i:
+                arr2 = arr2.repeat(sz, axis=j) 
+        ans.append(arr2)
+
+    #return tuple(ans)
+    return tuple(ans[::-1])
+
+def multi_dim_flatgrid(*arrs):
+    """
+    Creates flattened versions of meshgrids.
+    Returns a tuple of arrays of values of individual parameters
+    at each point in a grid, returned in a flat array structure.
+
+    e.g.
+    x = [1,3,5]
+    y = [2,4,6]
+    X, Y = multi_dim_flatgrid(x, y)
+    returns:
+    X = [1,1,1,3,3,3,5,5,5]
+    Y = [2,4,6,2,4,6,2,4,6]
+    """
+    outarrs = multi_dim_meshgrid(*arrs)
+    return tuple([ outarrs[i].flatten() for i in xrange(len(outarrs)) ])
+
+def multi_dim_grid(*arrs):
+    """
+    Creates an array of values of all pts on a multidimensional grid.
+
+    e.g.
+    x = [1,3,5]
+    y = [2,4,6]
+    multi_dim_grid(x, y)
+    returns:
+    [[1,2], [1,4], [1,6],
+     [3,2], [3,4], [3,6],
+     [5,2], [5,4], [5,6]]
+    """
+    temp = multi_dim_flatgrid(*arrs)
+    return np.transpose( np.array(temp) )
+
+
+#
+# Routines for least-squares fit
+#
+def residuals2d(gamma, y, x1, x2):
+    g11 = gamma[0]
+    g12 = gamma[1]
+    g22 = gamma[2]
+    return y - (1. - g11*x1*x1/2. - g12*x1*x2 - g22*x2*x2/2.)
+
+def evalfit2d(x1, x2, gamma):
+    g11 = gamma[0]
+    g12 = gamma[1]
+    g22 = gamma[2]
+    return 1. - g11*x1*x1/2. - g12*x1*x2 - g22*x2*x2/2.
+
---
+++ MonteCarloMarginalizeCode/Code/test_effectiveFisher.py
@@ -0,0 +1,62 @@
+from effectiveFisher import *
+import matplotlib.pyplot as plt
+
+# Setup signal and IP class
+m1=30.*lal.LAL_MSUN_SI
+m2=4.2*lal.LAL_MSUN_SI
+PSIG = ChooseWaveformParams(m1=m1, m2=m2, deltaF=1./16., approx=lalsim.EOBNRv2)
+#PSIG = ChooseWaveformParams(m1=m1, m2=m2, deltaF=1./16., approx=lalsim.TaylorT1)
+IP = Overlap(fLow=40., deltaF=1./16., psd=lal.LIGOIPsd)
+PTMPLT = PSIG.copy()
+hfSIG = norm_hoff(PSIG, IP)
+McSIG = mchirp(m1, m2)
+etaSIG = symRatio(m1, m2)
+minMc = McSIG - 0.04*lal.LAL_MSUN_SI
+maxMc = McSIG + 0.04*lal.LAL_MSUN_SI
+minEta = max(0.01, etaSIG-0.005)
+maxEta = min(0.25, etaSIG+0.005)
+NMcs = 21
+NEtas = 21
+
+# setup parameter grid
+param_names = ['Mc', 'eta']
+Mcrange = [minMc, maxMc]
+etarange = [minEta, maxEta]
+param_ranges = [Mcrange, etarange]
+pts_per_dim = [NMcs, NEtas]
+Mcpts, etapts = make_regular_1d_grids(param_ranges, pts_per_dim)
+McMESH, etaMESH =multi_dim_meshgrid(Mcpts, etapts)
+McFLAT, etaFLAT = multi_dim_flatgrid(Mcpts, etapts)
+dMcMESH = McMESH - McSIG
+detaMESH = etaMESH - etaSIG
+dMcFLAT = McFLAT - McSIG
+detaFLAT = etaFLAT - etaSIG
+grid = multi_dim_grid(Mcpts, etapts)
+#McMESH /= lal.LAL_MSUN_SI
+#dMcMESH /= lal.LAL_MSUN_SI
+
+# Evaluate ambiguity function on the grid
+rhos = np.array(evaluate_ip_on_grid(hfSIG, PTMPLT, IP, param_names, grid))
+rhogrid = rhos.reshape(NMcs, NEtas)
+
+# Fit to determine effective Fisher matrix
+cut = rhos > 0.99
+fitgamma = effectiveFisher(residuals2d, rhos[cut], dMcFLAT[cut], detaFLAT[cut])
+print "Least squares fit finds g_Mc,Mc = ", fitgamma[0]
+print "                        g_Mc,eta = ", fitgamma[1]
+print "                        g_eta,eta = ", fitgamma[2]
+
+
+plt.figure(1)
+cntrs = [.97,0.975,0.98,0.985,0.99,0.995,1.0]
+plt.contourf(McMESH, etaMESH, rhogrid, cntrs, cmap=plt.cm.jet)
+plt.scatter(McSIG,etaSIG,marker='x',c='k',s=40)
+plt.xlabel("Mc")
+plt.ylabel("eta")
+
+plt.figure(2)
+plt.contourf(dMcMESH, detaMESH,
+        evalfit2d(dMcMESH, detaMESH, fitgamma),
+        cntrs, cmap=plt.cm.jet)
+plt.scatter(0.,0.,marker='x',c='k',s=40)
+plt.show()
