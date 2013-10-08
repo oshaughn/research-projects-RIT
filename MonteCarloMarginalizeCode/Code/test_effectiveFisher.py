883e3e6c653e0b0192442a43600b5d05a7bd2415
Evan Ochsner <evan.ochsner@ligo.org>
2013-10-08 17:39:31 -0500

    Cleanup test_effectiveFisher.py code

--- MonteCarloMarginalizeCode/Code/test_effectiveFisher.py
+++ MonteCarloMarginalizeCode/Code/test_effectiveFisher.py
@@ -1,14 +1,17 @@
 from effectiveFisher import *
 import matplotlib.pyplot as plt
-import time
+from time import clock
+from functools import partial
 
-start = time.clock()
+start = clock()
+elapsed_time = lambda: clock()-start
 
 # Setup signal and IP class
 m1=10.*lal.LAL_MSUN_SI
-m2=1.4*lal.LAL_MSUN_SI
+m2=10.*lal.LAL_MSUN_SI
 PSIG = ChooseWaveformParams(m1=m1, m2=m2, approx=lalsim.TaylorT1)
-PSIG.deltaF = findDeltaF(PSIG)
+#PSIG.deltaF = findDeltaF(PSIG)
+PSIG.deltaF = 1./8.
 IP = Overlap(fLow=40., deltaF=PSIG.deltaF, psd=lal.LIGOIPsd)
 PTMPLT = PSIG.copy()
 hfSIG = norm_hoff(PSIG, IP)
@@ -16,10 +19,6 @@ McSIG = mchirp(m1, m2)
 etaSIG = symRatio(m1, m2)
 NMcs = 11
 NEtas = 11
-#minMc = McSIG - 0.04*lal.LAL_MSUN_SI
-#maxMc = McSIG + 0.04*lal.LAL_MSUN_SI
-#minEta = max(0.01, etaSIG-0.005)
-#maxEta = min(0.25, etaSIG+0.005)
 param_names = ['Mc', 'eta']
 
 # Find appropriate parameter ranges
@@ -33,15 +32,13 @@ for i, param in enumerate(param_names):
     else:
         print "\t", param, ":", param_ranges[i]
 
-elapsed = (time.clock() - start)
-print "Range-finding took:", elapsed, "(s) for", len(param_names), "parameters"
+elapsed = elapsed_time()
+print "Range-finding took:", elapsed, "(s) for", len(param_names),"parameters\n"
 
-# setup parameter grid
-#Mcrange = [minMc, maxMc]
-#etarange = [minEta, maxEta]
-#param_ranges = [Mcrange, etarange]
+# setup uniform parameter grid for effective Fisher
 pts_per_dim = [NMcs, NEtas]
 Mcpts, etapts = make_regular_1d_grids(param_ranges, pts_per_dim)
+etapts = map(sanitize_eta, etapts)
 McMESH, etaMESH =multi_dim_meshgrid(Mcpts, etapts)
 McFLAT, etaFLAT = multi_dim_flatgrid(Mcpts, etapts)
 dMcMESH = McMESH - McSIG
@@ -64,12 +61,20 @@ rhogrid = rhos.reshape(NMcs, NEtas)
 cut = rhos > 0.99
 fitgamma = effectiveFisher(residuals2d, rhos[cut], dMcFLAT_MSUN[cut],
         detaFLAT[cut])
+# Find the eigenvalues/vectors of the effective Fisher matrix
+gam = array_to_symmetric_matrix(fitgamma)
+evals, evecs, rot = eigensystem(gam)
+
+elapsed = elapsed_time() - elapsed
+print "Time to compute Fisher matrix and its eigensystem:", elapsed
+print "For a grid of size:", pts_per_dim, "\n"
+
+# Print information about the effective Fisher matrix
+# and its eigensystem
 print "Least squares fit finds g_Mc,Mc = ", fitgamma[0]
 print "                        g_Mc,eta = ", fitgamma[1]
 print "                        g_eta,eta = ", fitgamma[2]
 
-gam = array_to_symmetric_matrix(fitgamma)
-evals, evecs, rot = eigensystem(gam)
 print "\nFisher matrix:"
 print "eigenvalues:", evals
 print "eigenvectors:"
@@ -77,38 +82,49 @@ print evecs
 print "rotation taking eigenvectors into Mc, eta basis:"
 print rot
 
-cov = inv(gam)
-evals2, evecs2, rot2 = eigensystem(cov)
-print "\nCovariance matrix:"
-print "eigenvalues:", evals2
-print "eigenvectors:"
-print evecs2
-print "rotation taking eigenvectors into Mc, eta basis:"
-print rot2
-
-elapsed = (time.clock() - elapsed)
-print "Time to compute Fisher matrix and its eigensystem:", elapsed
-print "For a grid of size:", pts_per_dim
-
-# Check overlap at a bunch of pts inside predicted 0.97 contour
+#
+# Distribute points inside predicted ellipsoid of certain level of overlap
+#
 match_cntr = 0.97
 Nrandpts=200
-r1 = np.sqrt(2.*(1.-match_cntr)/evals[0])
+r1 = np.sqrt(2.*(1.-match_cntr)/evals[0]) # ellipse radii along eigendirections
 r2 = np.sqrt(2.*(1.-match_cntr)/evals[1])
 # Get pts. inside an ellipsoid oriented along eigenvectors...
 rand_grid = uniform_random_ellipsoid(Nrandpts, r1, r2)
 # Rotate to get coordinates in parameter basis
-#rand_grid = np.array([ np.real(evecs.dot(rand_grid[i]))
 rand_grid = np.array([ np.real(rot.dot(rand_grid[i]))
     for i in xrange(len(rand_grid)) ])
-# Put in convenient units, subtract off true param values, etc.
-rand_dMcs_MSUN, rand_detas = tuple(np.transpose(rand_grid))
-rand_Mcs = rand_dMcs_MSUN * lal.LAL_MSUN_SI + McSIG
-rand_etas = rand_detas + etaSIG
+# Put in convenient units,
+# change from parameter differential (i.e. dtheta)
+# to absolute parameter value (i.e. theta = theta_true + dtheta)
+rand_dMcs_MSUN, rand_detas = tuple(np.transpose(rand_grid)) # dMc, deta
+rand_Mcs = rand_dMcs_MSUN * lal.LAL_MSUN_SI + McSIG # Mc (kg)
+rand_etas = rand_detas + etaSIG # eta
+
+# Prune points with unphysical values of eta from rand_grid
+rand_etas = np.array(map(partial(sanitize_eta, exception=np.NAN), rand_etas))
 rand_grid = np.transpose((rand_Mcs,rand_etas))
-# Evaluate IP at random pts in the ellipsoid
+phys_cut = ~np.isnan(rand_grid).any(1) # cut to remove unphysical pts
+rand_grid = rand_grid[phys_cut]
+print "Requested", Nrandpts, "points inside the ellipsoid of",\
+        match_cntr, "match."
+print "Kept", len(rand_grid), "points with physically allowed parameters."
+
+
+#
+# N.B. Below here, the real code will divy up rand_grid into blocks of intrinsic
+# parameters and compute the marginalized likelihood on each point
+# in intrinsic parameter space
+#
+# For testing purposes, simply evaluate the overlap on rand_grid and plot
+# to confirm it is placing points in the expected ellipsoid with the
+# proper distribution, and the overlap behaves properly.
+#
+
+# Evaluate IP on the grid inside the ellipsoid
 rhos2 = evaluate_ip_on_grid(hfSIG, PTMPLT, IP, param_names, rand_grid)
 
+# Plot the ambiguity function, effective Fisher and ellipsoid points
 plt.figure(1)
 plt.title('Ambiguity function')
 cntrs = [.97,0.975,0.98,0.985,0.99,0.995,1.0]
@@ -130,7 +146,7 @@ plt.ylabel("$\Delta \eta$")
 
 plt.figure(3)
 plt.title('Random sampling of 0.97 ellipsoid')
-plt.scatter(rand_dMcs_MSUN, rand_detas, c=rhos2, cmap=plt.cm.jet)
+plt.scatter(rand_dMcs_MSUN[phys_cut], rand_detas[phys_cut], c=rhos2, cmap=plt.cm.jet)
 plt.colorbar()
 plt.xlabel("$\Delta M_c$")
 plt.ylabel("$\Delta \eta$")
