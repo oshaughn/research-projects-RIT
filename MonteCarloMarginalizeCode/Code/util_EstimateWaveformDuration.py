70b94c7bdbf1ded6beab0ab931f16dbcd8a2f418
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2013-12-20 13:54:13 -0600

    Add util_EstimateWaveformDuration.py

---
+++ MonteCarloMarginalizeCode/Code/util_EstimateWaveformDuration.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import lalsimutils
+import ourparams
+opts,  rosDebugMessagesDictionary = ourparams.ParseStandardArguments()
+lalsimutils.rosDebugMessagesDictionary            = rosDebugMessagesDictionary
+
+
+Psig = ourparams.PopulatePrototypeSignal(opts)
+
+timeWaveform = lalsimutils.estimateWaveformDuration(Psig)
+
+print float(timeWaveform)
