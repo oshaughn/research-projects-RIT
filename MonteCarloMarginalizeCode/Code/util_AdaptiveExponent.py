35d3d27165006f8d14b938a1bdb55a64050cf0ff
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2014-01-01 05:47:05 -0600

    util_AdaptiveExponent.py: For injections, provide code path to compute SNRs and thereby better estimate the peak lnL, by explicitly generating the signal and using the PSDs. (Slow.  This should be part of the main code, eventually)

--- MonteCarloMarginalizeCode/Code/util_AdaptiveExponent.py
+++ MonteCarloMarginalizeCode/Code/util_AdaptiveExponent.py
@@ -15,16 +15,72 @@ if opts.coinc:
         rho2Net += rhoExpected[det]*rhoExpected[det]
 
 
-if opts.inj:
+if opts.inj and opts.channel_name:
     # Read injection parameters
     Psig = ourparams.PopulatePrototypeSignal(opts)
+    # Make fake signal.  Use one instrument.
+    import factored_likelihood
+    import lalsimulation as lalsim
+    import lalsimutils
+
+    data_fake_dict ={}
+    psd_dict = {}
+    rhoFake = {}
+    fminSNR =opts.fmin_SNR
+    fmaxSNR =opts.fmax_SNR
+    fSample = opts.srate
+    fNyq = fSample/2.
+    rho2Net =0
+    
+    for det, chan in map(lambda c: c.split("="), opts.channel_name):
+        Psig.detector = det
+        data_fake_dict[det] = factored_likelihood.non_herm_hoff(Psig)
+
+        deltaF = data_fake_dict[det].deltaF
+
+        # Read in psd to compute range.  
+        if not(opts.psd_file) and not(opts.psd_file_singleifo):
+            analyticPSD_Q = True # For simplicity, using an analytic PSD
+            psd_dict[det] = lalsim.SimNoisePSDiLIGOSRD   
+        else:
+            if not (opts.psd_file_singleifo):
+                analyticPSD_Q = False # For simplicity, using an analytic PSD
+                # "Standard" PSD parsing code used on master.
+                psd_dict[det] = lalsimutils.get_psd_series_from_xmldoc(opts.psd_file, det)  # pylal type!
+                fmin = psd_dict[det].f0
+                fmax = fmin + psd_dict[det].deltaF*len(psd_dict[det].data)-deltaF
+                psd_dict[det] = lalsimutils.resample_psd_series(psd_dict[det], deltaF)
+            else: # use list-based procedure to load in individual PSDs for individual instruments
+                for inst, psdf in map(lambda c: c.split("="), opts.psd_file_singleifo):
+                    if inst is det:
+                        psd_dict[det] = lalsimutils.get_psd_series_from_xmldoc(psdf, det)  # pylal type!
+                        fmin = psd_dict[det].f0
+                        fmax = fmin + psd_dict[det].deltaF*len(psd_dict[det].data)-deltaF
+                        psd_dict[det] = lalsimutils.resample_psd_series(psd_dict[det], deltaF)
+                        
+
+            
+
+    for det in data_fake_dict.keys():
+        # Create inner product for the detector. Good enough to compute SNR.
+        if analyticPSD_Q:
+            IP = lalsimutils.ComplexIP(fLow=fminSNR, fNyq=fSample/2,deltaF=deltaF,psd=psd_dict[det],fMax=fmaxSNR, analyticPSD_Q=analyticPSD_Q)
+        else:
+            IP = lalsimutils.ComplexIP(fLow=fminSNR, fNyq=fSample/2,deltaF=deltaF,psd=psd_dict[det].data.data,fMax=fmaxSNR, analyticPSD_Q=analyticPSD_Q)
+        rhoFake[det] = IP.norm(data_fake_dict[det])   # Reset
+
+        print rhoFake[det]
+        # Construct rho^2
+        rho2Net += rhoFake[det]*rhoFake[det]
+
+
     # Calculate the SNRs, using the PSDs provided? Not now...
     # ...or estimate it, from the masses provided.  Assume an aLIGO-scale range.  Ad-hoc angle-average and factors!
-    rho = 8.* ((Psig.m1 + Psig.m2)/lal.LAL_MSUN_SI/1.2) * Psig.dist/(200*1e6*lal.LAL_PC_SI)
-    rho2Net = rho*rho
+    #    rho = 8.* ((Psig.m1 + Psig.m2)/lal.LAL_MSUN_SI/2.4) * Psig.dist/(200*1e6*lal.LAL_PC_SI)
+    #    rho2Net = rho*rho
 
 # Estimate the 'beta' parameter needed to regularize the likelihood, so even the max-likelihood event doesn't overwhelm the histogram
 #    exp(rho^2/2)^beta ~ nchunk*10
 # Add an ad-hoc x2 to be in better agreement with manual calibration for zero-noise MDC event
-print np.min([1,2* np.log(opts.nskip*10)*2./rho2Net])
+print np.min([0.8,2* np.log(opts.nskip*10)*2./rho2Net])
 sys.exit(0)
