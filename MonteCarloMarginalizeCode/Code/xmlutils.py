41b90195215276ddba589fb40a4c4d991de87e97
Chris Pankow <chris.pankow@ligo.org>
2014-02-21 03:39:00 -0500

    xmlutils.py: Rename inner loop variable to remove any chance of scope confusion.

--- MonteCarloMarginalizeCode/Code/xmlutils.py
+++ MonteCarloMarginalizeCode/Code/xmlutils.py
@@ -64,7 +64,7 @@ def append_samples_to_xmldoc(xmldoc, sampdict):
     # parameters across the transpose operation. It's probably not necessary,
     # so if speed dictates, it can be reworked by flattening before arriving 
     # here
-    for vrow in numpy.array(zip(*[vrow.T for vrow in values]), dtype=numpy.object):
+    for vrow in numpy.array(zip(*[vrow_sub.T for vrow_sub in values]), dtype=numpy.object):
         #si_table.append(samples_to_siminsp_row(si_table, **dict(zip(keys, vrow.flatten()))))
         vrow = reduce(list.__add__, [list(i) if isinstance(i, collections.Iterable) else [i] for i in vrow])
         si_table.append(samples_to_siminsp_row(si_table, **dict(zip(keys, vrow))))
