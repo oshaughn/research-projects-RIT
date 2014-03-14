9d8c7222e0b5b2374cae1cb8cd1e86cb4fa36d0d
Evan Ochsner <evan.ochsner@ligo.org>
2014-03-14 11:23:18 -0500

    Updated runtime plot and sparkler

--- MonteCarloMarginalizeCode/Notes/Presentations/make_fmin_scaling_plot.py
+++ MonteCarloMarginalizeCode/Notes/Presentations/make_fmin_scaling_plot.py
@@ -1,10 +1,13 @@
 import numpy as np
 from matplotlib import pylab as plt
 
-ILE_fmins = [20., 25., 30., 35., 40.]
-ILE_data = [2674., 2174., 2162., 1892., 1890.] # runtimes in s (avg of 2 runs)
+#ILE_fmins = [10., 15., 20., 25., 30., 35., 40.]
+ILE_data = [8664., 2172., 2674., 2174., 2162., 1892., 1890.] # runtimes in s (avg of 2 runs)
+ILE_fmins = [15., 20., 25., 30., 35., 40.]
+T1_data = [2195., 2679., 2124., 2189., 1885., 1861.]
+EOB_data = [2202., 2407., 1859., 1847., 1584., 1564.]
 
-rt_40 = 1890. # runtime of Bayesian PE using fmin=40.
+rt_40 = 1861. # runtime of Bayesian PE using fmin=40.
 
 def Bayes_runtime(fmin, rt_40):
     """
@@ -15,8 +18,10 @@ def Bayes_runtime(fmin, rt_40):
 
 fmins = np.arange(10., 40., 0.1)
 
-plt.semilogy(fmins, Bayes_runtime(fmins, rt_40), label="Waveform-limited scaling")
-plt.scatter(ILE_fmins, ILE_data, label="rapid PE")
+plt.semilogy(fmins, Bayes_runtime(fmins, rt_40), 'k-', label="Waveform-limited scaling")
+plt.scatter(ILE_fmins, T1_data, label="rapid PE (T1)", c='b')
+plt.scatter(ILE_fmins, EOB_data, label="rapid PE (EOBNRv2)", marker='x', c='r')
+plt.title('PE Runtimes for $(1.55+1.23) M_\odot$ BNS signal')
 plt.legend()
 plt.xlabel("min. freq. (Hz)")
 plt.ylabel("runtime (s)")
