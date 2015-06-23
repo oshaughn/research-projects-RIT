e8ac09acc2c6a0dc6edf84273c933b4f02de65d9
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2015-06-23 16:18:26 -0400

    util_WriteFrameAndCacheFromXML.sh: Useful new tool

--- MonteCarloMarginalizeCode/Code/util_WriteFrameAndCacheFromXML.sh
+++ MonteCarloMarginalizeCode/Code/util_WriteFrameAndCacheFromXML.sh
@@ -2,16 +2,28 @@
 #
 # Uses a fiducial event time, start time, stop time
 #
-#  util_WriteFrameAndCacheFromXML.sh   <inj> <event> <base>
+#  util_WriteFrameAndCacheFromXML.sh   <inj> <event> <base>  [<approx>
+#
+#     <approx>    [TaylorT4|MATLAB|EOBNRv2|...]  # approximant used
+#
+# EXAMPLES
+#   util_WriteFrameAndCacheFromXML.sh mdc.xml.gz 0 BASE
+#   util_WriteFrameAndCacheFromXML.sh mdc.xml.gz 0 BASE MATLAB
+#   util_WriteFrameAndCacheFromXML.sh mdc.xml.gz 0 BASE EOBNRv2
 
 PI=`which util_PrintInjection.py`
 ED=`which util_EstimateWaveformDuration.py`
 LWF=`which util_LALWriteFrame.py`                # Can switch to EOBWriteFrame if necessary
-EWF=`which util_EOBWriteFrame.py`                # Can switch to EOBWriteFrame if necessary
+EWF=`which util_EOBTideWriteFrame.py`                # Can switch to EOBWriteFrame if necessary
 echo Using PrintInjection ${PI}
 echo Using EstimateDuration ${ED}
 echo Using LALWriteFrame ${LWF}
 
+if [ "${PI}" == ''  ]; 
+then
+ echo " Cannot find executables; failing "
+ exit 1
+fi
 
 get_abs_filename() {
   # $1 : relative filename
@@ -25,29 +37,59 @@ EVENT=$2
 BASE=$3
 ${PI} --inj ${INJ_XML} --event ${EVENT} --verbose
 
+# Last argument:
+#    - approximant
+#    - MATLAB (for Bernuzzi)
+#    
+APPROX=TaylorT4
+if [ "$#" -gt 3 ]; then
+  echo " Processing approximant ", $4
+  if [ $4 == "MATLAB" ]; then
+      echo " -------    USING EOB MODEL   -------  "
+      LWF=${EWF}
+  else
+      APPROX=$4
+  fi
+fi
+
+if [ "${LWF}" == ''  ]; 
+then
+ echo " Cannot find executables; failing "
+ exit 1
+fi
+
 
 INJ_CHANNEL_NAME=FAKE-STRAIN
 export DUR_EST=`${ED} --inj-xml ${INJ_XML} --event ${EVENT} | tail -n 1`
 export DUR=`python -c "import math; print 2**(2+int(math.log(${DUR_EST}*2)/math.log(2)))"`  # use a longer window
-echo " Estimated duration " , ${DUR_EST}
-echo " Target buffer length ", ${DUR}
+echo " Estimated duration "   ${DUR_EST}
+echo " Target buffer length "  ${DUR}
+if [ "${DUR_EST}" == '' ]; 
+then
+ echo " Cannot estimate duration failing "
+ exit 1
+fi
 
-EVENT_TIME=`util_PrintInjection.py --inj mdc.xml.gz  --event 0 --verbose | grep coal | awk '{print $6}'`
-START=`python -c "print int(${EVENT_TIME}-${DUR}-64)"`
-STOP=`python -c "print int(${EVENT_TIME}+${DUR}+64)"`
 
-echo " Duration estimates :"  ${START} ${STOP} ${DUR} ${DUR_EST} 
+
+EVENT_TIME=`${PI} --inj mdc.xml.gz  --event 0 --verbose | grep coal | awk '{print $6}'`
+# Modified so the buffer has a power-of-2 length
+START=`python -c "print int(${EVENT_TIME}-${DUR})"`
+STOP=`python -c "print int(${EVENT_TIME}+${DUR})"`
+DUR_REVISED=`python -c "print int(${STOP}-${START})"`
+
+echo " Duration estimates :"  ${START} ${STOP}  ${DUR_EST} ${DUR} ${DUR_REVISED}
 
 function make_cache(){
-  local ifo=$1
-  local base=$2
-  local START=$3
-  local STOP=$4
-  local INJ_XML=$5
-  local EVENT=$6
-  local MIN_DUR=$7
+  local ifo=$1;
+  local base=$2;
+  local START=$3;
+  local STOP=$4;
+  local INJ_XML=$5;
+  local EVENT=$6;
+  local MIN_DUR=$7;
   mkdir ${base}_mdc;
-  (cd ${base}_mdc;  ${LWF} --start ${START} --stop ${STOP} --inj ${INJ_XML} --event ${EVENT} --single-ifo --instrument ${ifo}1 --seglen ${DUR})
+  (cd ${base}_mdc;  ${LWF} --start ${START} --stop ${STOP} --inj ${INJ_XML} --event ${EVENT} --single-ifo --instrument ${ifo}1  --approx ${APPROX})
 #  find ${base}_mdc/ -name  ${ifo}"-*.gwf" | lalapps_path2cache > ${ifo}.cache
 }
 
