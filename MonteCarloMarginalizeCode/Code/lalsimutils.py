2ed567b0a8808c80b0fa68e6b6af383821cbbd0c
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2016-11-29 09:22:59 -0500

    EOBNRv2 added (i.e., no HM), via call to SEOB_dict

--- MonteCarloMarginalizeCode/Code/lalsimutils.py
+++ MonteCarloMarginalizeCode/Code/lalsimutils.py
@@ -1959,7 +1959,7 @@ def hlmoft(P, Lmax=2):
     """
     assert Lmax >= 2
 
-    if (P.approx == lalsim.SEOBNRv2 or P.approx == lalsim.SEOBNRv1 or P.approx == lalSEOBv4):
+    if (P.approx == lalsim.SEOBNRv2 or P.approx == lalsim.SEOBNRv1 or P.approx == lalSEOBv4 or P.approx == lalsim.EOBNRv2):
         hlm_out = hlmoft_SEOB_dict(P)
         if True: #P.taper:
             ntaper = int(0.01*hlm_out[(2,2)].data.length)  # fixed 1% of waveform length, at start
@@ -2024,12 +2024,12 @@ def hlmoft_SEOB_dict(P,Lmax=2):
     """
     Generate the TD h_lm -2-spin-weighted spherical harmonic modes of a GW
     with parameters P. Returns a dictionary of modes.
-    Just for SEOBNRv2HM and SEOBNRv1HM.  Uses aligned-spin trick to get (2,2) and (2,-2) modes.
+    Just for SEOBNRv2 SEOBNRv1, and EOBNRv2.  Uses aligned-spin trick to get (2,2) and (2,-2) modes.
     A hack.
     Works for any aligned-spin time-domain waveform with only (2,\pm 2) modes though.
     """
 
-    if not (P.approx == lalsim.SEOBNRv2 or P.approx==lalsim.SEOBNRv1 or P.approx == lalSEOBv4):
+    if not (P.approx == lalsim.SEOBNRv2 or P.approx==lalsim.SEOBNRv1 or P.approx == lalSEOBv4 or P.approx=lalsim.EOBNRv2 ):
         return None
 
     # Remember, we have a fiducial orientation for the h22. 
