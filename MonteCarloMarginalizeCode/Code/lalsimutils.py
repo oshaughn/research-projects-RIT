lalsimutils.py: Bug in *tidal conversion* (sign error).

MonteCarloMarginalizeCode/Code/lalsimutils.py
@@ -1721,6 +1721,7 @@ def Mceta(m1, m2):
 def tidal_lambda_tilde(mass1, mass2, lambda1, lambda2):
     """
     'Effective' lambda parameters.
+    Lackey et al https://arxiv.org/pdf/1402.5156.pdf, Eq. (5,6).
     """
     mt = mass1 + mass2
     eta = mass1 * mass2 / mt**2
@@ -1729,7 +1730,7 @@ def tidal_lambda_tilde(mass1, mass2, lambda1, lambda2):
     lt_sym = lt1 + lt2
     lt_asym = lt1 - lt2
 
-    lam_til = (1 + 7*eta - 31*eta**2) * lt_sym - q * (1 + 9*eta - 11*eta**2) * lt_asym
+    lam_til = (1 + 7*eta - 31*eta**2) * lt_sym + q * (1 + 9*eta - 11*eta**2) * lt_asym
     dlam_til = q * (1 - 13272*eta/1319 + 8944*eta**2/1319) * lt_sym + (1 - 15910*eta/1319 + 32850*eta**2/1319 + 3380*eta**3/1319) * lt_asym
     dlam_til *= 0.5
     lam_til *= 8. / 13
@@ -1741,15 +1742,16 @@ def tidal_lambda_from_tilde(mass1, mass2, lam_til, dlam_til):
     """
     mt = mass1 + mass2
     eta = mass1 * mass2 / mt**2
-    q = np.sqrt(1 - 4*eta)
+    # q = np.sqrt(1 - 4*eta)
 
-    a = (8./13) * (1 + 7*eta - 31*eta**2)
-    b = (8./13) * q * (1 + 9*eta - 11*eta**2)
-    c = 0.5 * q * (1 - 13272*eta/1319 + 8944*eta**2/1319)
-    d = 0.5 * (1 - 15910*eta/1319 + 32850*eta**2/1319 + 3380*eta**3/1319)
+    # a = (8./13) * (1 + 7*eta - 31*eta**2)
+    # b = (8./13) * q * (1 + 9*eta - 11*eta**2)
+    # c = 0.5 * q * (1 - 13272*eta/1319 + 8944*eta**2/1319)
+    # d = 0.5 * (1 - 15910*eta/1319 + 32850*eta**2/1319 + 3380*eta**3/1319)
 
-    lambda1 = 0.5 * ((c - d) * lam_til - (a - b) * dlam_til)/(b*c - a*d)
-    lambda2 = 0.5 * ((c + d) * lam_til - (a + b) * dlam_til)/(a*d - b*c)
+    # lambda1 = 0.5 * ((c - d) * lam_til - (a - b) * dlam_til)/(b*c - a*d)
+    # lambda2 = 0.5 * ((c + d) * lam_til - (a + b) * dlam_til)/(a*d - b*c)
+    lambda1,lambda2 = lam1_lam2_of_pe_params(eta, lam_til, dlam_til)
 
     return lambda1, lambda2
 