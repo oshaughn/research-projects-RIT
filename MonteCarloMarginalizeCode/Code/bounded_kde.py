aae9f11cc3e684a5d18dae9a088eb4e3438b2546
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2018-03-02 09:07:30 -0500

    bounded_kde.py: Added hardcoded 2d KDE class that is used fairly often elsewhere

--- MonteCarloMarginalizeCode/Code/bounded_kde.py
+++ MonteCarloMarginalizeCode/Code/bounded_kde.py
@@ -103,3 +103,107 @@ class BoundedKDE(gaussian_kde):
         construct the KDE that have a lower KDE density than ``pt``."""
 
         return np.count_nonzero(self(self.dataset) < self(pt)) / self.n
+
+
+
+# Special case
+class BoundedKDE_2d(gaussian_kde):
+    r"""Represents a two-dimensional Gaussian kernel density estimator
+    for a probability distribution function that exists on a bounded
+    domain."""
+
+    def __init__(self, pts, xlow=None, xhigh=None, ylow=None, yhigh=None, *args, **kwargs):
+        """Initialize with the given bounds.  Either ``low`` or
+        ``high`` may be ``None`` if the bounds are one-sided.  Extra
+        parameters are passed to :class:`gaussian_kde`.
+
+        :param xlow: The lower x domain boundary.
+
+        :param xhigh: The upper x domain boundary.
+
+        :param ylow: The lower y domain boundary.
+
+        :param yhigh: The upper y domain boundary.
+        """
+        pts = np.atleast_2d(pts)
+
+        assert pts.ndim == 2, 'Bounded_kde can only be two-dimensional'
+
+        super(BoundedKDE_2d, self).__init__(pts.T, *args, **kwargs)
+
+        self._xlow = xlow
+        self._xhigh = xhigh
+        self._ylow = ylow
+        self._yhigh = yhigh
+
+    @property
+    def xlow(self):
+        """The lower bound of the x domain."""
+        return self._xlow
+
+    @property
+    def xhigh(self):
+        """The upper bound of the x domain."""
+        return self._xhigh
+
+    @property
+    def ylow(self):
+        """The lower bound of the y domain."""
+        return self._ylow
+
+    @property
+    def yhigh(self):
+        """The upper bound of the y domain."""
+        return self._yhigh
+
+    def evaluate(self, pts):
+        """Return an estimate of the density evaluated at the given
+        points."""
+        pts = np.atleast_2d(pts)
+        assert pts.ndim == 2, 'points must be two-dimensional'
+
+        x, y = pts.T
+        pdf = super(BoundedKDE_2d, self).evaluate(pts.T)
+        if self.xlow is not None:
+            pdf += super(BoundedKDE_2d, self).evaluate([2*self.xlow - x, y])
+
+        if self.xhigh is not None:
+            pdf += super(BoundedKDE_2d, self).evaluate([2*self.xhigh - x, y])
+
+        if self.ylow is not None:
+            pdf += super(BoundedKDE_2d, self).evaluate([x, 2*self.ylow - y])
+
+        if self.yhigh is not None:
+            pdf += super(BoundedKDE_2d, self).evaluate([x, 2*self.yhigh - y])
+
+        if self.xlow is not None:
+            if self.ylow is not None:
+                pdf += super(BoundedKDE_2d, self).evaluate([2*self.xlow - x, 2*self.ylow - y])
+
+            if self.yhigh is not None:
+                pdf += super(BoundedKDE_2d, self).evaluate([2*self.xlow - x, 2*self.yhigh - y])
+
+        if self.xhigh is not None:
+            if self.ylow is not None:
+                pdf += super(BoundedKDE_2d, self).evaluate([2*self.xhigh - x, 2*self.ylow - y])
+            if self.yhigh is not None:
+                pdf += super(BoundedKDE_2d, self).evaluate([2*self.xhigh - x, 2*self.yhigh - y])
+
+        return pdf
+
+    def __call__(self, pts):
+        pts = np.atleast_2d(pts)
+        out_of_bounds = np.zeros(pts.shape[0], dtype='bool')
+
+        if self.xlow is not None:
+            out_of_bounds[pts[:, 0] < self.xlow] = True
+        if self.xhigh is not None:
+            out_of_bounds[pts[:, 0] > self.xhigh] = True
+        if self.ylow is not None:
+            out_of_bounds[pts[:, 1] < self.ylow] = True
+        if self.yhigh is not None:
+            out_of_bounds[pts[:, 1] > self.yhigh] = True
+
+        results = self.evaluate(pts)
+        results[out_of_bounds] = 0.
+        return results
