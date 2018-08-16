e19283b1f22f40d4d2ec3fd240b4ae7f0a29e7f7
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2018-08-16 08:02:09 -0400

    util_CIPdagPostprocess.sh: new packaging for EOS results (spectral)

---
+++ MonteCarloMarginalizeCode/Code/util_CIPdagPostprocess.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+#
+# GOAL
+#   - wrap up results from
+#         - create_postprocessing_event_dag.py  
+
+
+DIR_PROCESS=$1
+BASE_OUT=$2
+
+# join together the .dat files
+echo " Joining data files .... "
+find ${DIR_PROCESS} -name 'integral*_withpriorchange+annotation.dat' -exec cat {} \;  | sort | uniq | sort -r >  ${BASE_OUT}.integral_withprior
+find ${DIR_PROCESS} -name 'integral*[0-9]+annotation.dat' -exec cat {} \;  | sort | uniq | sort -r >  ${BASE_OUT}.integral
+
+
+
+# Manifest
+rm -f ${BASE_OUT}.manifest
+echo '#User:' `whoami` >>  ${BASE_OUT}.manifest
+echo '#Date:' `date` >>  ${BASE_OUT}.manifest
+echo '#Host:' `hostname -f` >>  ${BASE_OUT}.manifest
+echo '#Directory:' `pwd`/${DIR_PROCESS} >>  ${BASE_OUT}.manifest
+md5sum ${DIR_PROCESS}/*psd.xml.gz >> ${BASE_OUT}.manifest
+cat ${DIR_PROCESS}/command-single_fit.sh >>  ${BASE_OUT}.manifest  
+env >> ${BASE_OUT}.environment  
+
+# tar file
+tar cvzf ${BASE_OUT}.tgz ${BASE_OUT}.integral ${BASE_OUT}.integral_withprior  ${BASE_OUT}.manifest ${BASE_OUT}.environment
+
+exit 0 ;  # force end on success, for DAG
