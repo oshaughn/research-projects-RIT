394616f57a90591c41eec4a41513d2a611fc6a78
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2018-12-13 09:00:54 -0500

    lalsimutils.py: modify to use a lal Create operation, for safety

--- MonteCarloMarginalizeCode/Code/lalsimutils.py
+++ MonteCarloMarginalizeCode/Code/lalsimutils.py
@@ -2578,9 +2578,13 @@ def hlmoft_SEOB_dict(P,Lmax=2):
         for mode in mode_list_orig:
             mode_conj = (mode[0],-mode[1])
             if not mode_conj in hlms:
-                hT = hlms[mode].copy() # hopefully this works
-                hT.data.data = np.conj(hT.data.data)
-                hlms[mode_conj] = hT
+                hC = hlms[mode]
+                hC2 = lal.CreateCOMPLEX16TimeSeries("Complex h(t)", hC.epoch, hC.f0, 
+                                                    hC.deltaT, lsu_DimensionlessUnit, hC.data.length)
+                hC2.data.data =np.conj(hC.data.data)
+#                hT = hlms[mode].copy() # hopefully this works
+#                hT.data.data = np.conj(hT.data.data)
+                hlms[mode_conj] = hC2
         return hlms
 
     if not (P.approx == lalsim.SEOBNRv2 or P.approx==lalsim.SEOBNRv1 or P.approx == lalSEOBv4 or P.approx==lalsim.EOBNRv2 or P.approx == lalTEOBv2 or P.approx==lalTEOBv4):
