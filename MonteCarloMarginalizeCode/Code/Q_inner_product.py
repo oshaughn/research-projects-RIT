7c76a745a4954dbd4ac25383240f48d74cce6a1b
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2019-01-23 13:48:19 -0500

    Q_inner_product.py: Modify so code looks for cubin

--- MonteCarloMarginalizeCode/Code/Q_inner_product.py
+++ MonteCarloMarginalizeCode/Code/Q_inner_product.py
@@ -1,5 +1,8 @@
 from __future__ import division
 import cupy
+import os
+
+ILE_base = os.environ["ILE_CODE_PATH"]  # default: store code inside main repo. Maintainer controls.
 
 def Q_inner_product_cupy(Q, A, start_indices, window_size):
     num_time_points, num_lms = Q.shape
@@ -16,7 +19,7 @@ def Q_inner_product_cupy(Q, A, start_indices, window_size):
     )
 
     mod = cupy.cuda.function.Module()
-    mod.load_file("cuda_Q_inner_product.cubin")
+    mod.load_file(ILE_base+"/cuda_Q_inner_product.cubin")
     Q_prod_fn = mod.get_function("Q_inner")
 
     float_prec = 16
