1ef38e4065509f3167343dced1d8b2a01b543775
Leo Fang <leofang@bnl.gov>
2019-01-25 21:43:28 -0500

    switch to cupy.RawKernel() to avoid manual compiling

--- MonteCarloMarginalizeCode/Code/Q_inner_product.py
+++ MonteCarloMarginalizeCode/Code/Q_inner_product.py
@@ -2,7 +2,7 @@ from __future__ import division
 import cupy
 import os
 
-ILE_base = os.environ["ILE_CODE_PATH"]  # default: store code inside main repo. Maintainer controls.
+#ILE_base = os.environ["ILE_CODE_PATH"]  # default: store code inside main repo. Maintainer controls.
 
 def Q_inner_product_cupy(Q, A, start_indices, window_size):
     num_time_points, num_lms = Q.shape
@@ -11,16 +11,18 @@ def Q_inner_product_cupy(Q, A, start_indices, window_size):
     assert not cupy.isfortran(Q)
     assert not cupy.isfortran(A)
 
-
     out = cupy.empty(
         (num_extrinsic_samples, window_size),
         dtype=cupy.complex128,
         order="C",
     )
 
-    mod = cupy.cuda.function.Module()
-    mod.load_file(ILE_base+"/cuda_Q_inner_product.cubin")
-    Q_prod_fn = mod.get_function("Q_inner")
+    # it's assumed that cuda_Q_inner_product.cu is placed in the same folder as this code
+    path = os.path.join(os.path.dirname(__file__), 'cuda_Q_inner_product.cu')
+    with open('cuda_Q_inner_product.cu', 'r') as f:
+        code = f.read()
+        # actual compilation will happen only once, and after that RawKernel looks for cache first 
+        Q_prod_fn = cupy.RawKernel(code, "Q_inner")
 
     float_prec = 16
     num_threads_x = 4
