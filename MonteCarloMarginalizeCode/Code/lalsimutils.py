b45d01c0f6714f092334ce2220e119a2ae28b36c
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-02-15 06:13:36 -0700

    lalsimutils.py: Modify hlmoft to have logic to call v4HM

--- MonteCarloMarginalizeCode/Code/lalsimutils.py
+++ MonteCarloMarginalizeCode/Code/lalsimutils.py
@@ -2485,7 +2485,7 @@ def hlmoft(P, Lmax=2,nr_polarization_convention=False ):
     if nr_polarization_convention:
         sign_factor = -1
 
-    if (P.approx == lalsim.SEOBNRv2 or P.approx == lalsim.SEOBNRv1 or P.approx == lalSEOBv4 or P.approx == lalsim.EOBNRv2 or P.approx == lalTEOBv2 or P.approx==lalTEOBv4):
+    if (P.approx == lalsim.SEOBNRv2 or P.approx == lalsim.SEOBNRv1 or P.approx == lalSEOBv4 or P.approx == lalsim.EOBNRv2 or P.approx == lalTEOBv2 or P.approx==lalTEOBv4 or P.approx == lalSEOBNRv4HM):
         hlm_out = hlmoft_SEOB_dict(P)
         if True: #P.taper:
             ntaper = int(0.01*hlm_out[(2,2)].data.length)  # fixed 1% of waveform length, at start
