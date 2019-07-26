ee107a8cbb7efe7ded7c15cd633a8ea7ed242e00
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-07-26 10:50:35 -0400

    dag_utils.py: hardcode path to gzip

--- MonteCarloMarginalizeCode/Code/dag_utils.py
+++ MonteCarloMarginalizeCode/Code/dag_utils.py
@@ -1555,7 +1555,7 @@ def write_convertpsd_sub(tag='convert_psd', exe=None, ifo=None,file_input=None,t
     return ile_job, ile_sub_name
 
 
-def write_joingrids_sub(tag='join_grids', exe=None, input_pattern=None,target_dir=None,output_base=None,log_dir=None,n_explode=1, old_add=True, **kwargs):
+def write_joingrids_sub(tag='join_grids', exe=None, input_pattern=None,target_dir=None,output_base=None,log_dir=None,n_explode=1, gzip="/usr/bin/gzip", old_add=True, **kwargs):
     """
     Write script to convert PSD from one format to another.  Needs to be called once per PSD file being used.
     """
@@ -1589,7 +1589,7 @@ def write_joingrids_sub(tag='join_grids', exe=None, input_pattern=None,target_di
     fname_out =target_dir + "/" +output_base + ".xml"
     ile_job.set_stdout_file(fname_out)
 
-    ile_job.add_condor_cmd("+PostCmd", ' "gzip ' +fname_out + '"')
+    ile_job.add_condor_cmd("+PostCmd",  ' "' + gzip + ' ' +fname_out + '"')
 
     explode_str = ""
     for indx in np.arange(n_explode):
