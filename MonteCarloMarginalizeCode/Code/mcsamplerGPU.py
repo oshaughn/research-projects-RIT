7c3a62282ce12f41b9c00d97f8244e3f3920ecc5
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-08-30 10:55:35 -0400

    mcsamplerGPU: force arg cast to int

--- MonteCarloMarginalizeCode/Code/mcsamplerGPU.py
+++ MonteCarloMarginalizeCode/Code/mcsamplerGPU.py
@@ -501,7 +501,7 @@ class MCSampler(object):
         #
         nmax = kwargs["nmax"] if kwargs.has_key("nmax") else float("inf")
         neff = kwargs["neff"] if kwargs.has_key("neff") else numpy.float128("inf")
-        n = kwargs["n"] if kwargs.has_key("n") else min(1000, nmax)
+        n = int(kwargs["n"] if kwargs.has_key("n") else min(1000, nmax))
         convergence_tests = kwargs["convergence_tests"] if kwargs.has_key("convergence_tests") else None
 	save_no_samples = kwargs["save_no_samples"] if kwargs.has_key("save_no_samples") else None
 
