171cc28de071fd9bbb5105843732a5efc9b102fe
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-08-30 06:10:32 -0400

    mcsamplerGPU: GPU-enabled monte carlo adaptive integration

---
+++ MonteCarloMarginalizeCode/Code/vectorized_general_tools.py
@@ -0,0 +1,18 @@
+# Dan Wysocki
+
+import numpy as np
+import numpy
+
+def histogram(samples, n_bins, xpy=numpy):
+    n_samples = samples.size
+
+    # Compute the histogram counts.
+    indices = xpy.trunc(samples * n_bins).astype(np.int32)
+    histogram_counts = xpy.bincount(
+        indices, minlength=n_bins,
+        weights=xpy.broadcast_to(
+            xpy.asarray([float(n_bins)/n_samples]),
+            (n_samples,),
+        ),
+    )
+    return histogram_counts
