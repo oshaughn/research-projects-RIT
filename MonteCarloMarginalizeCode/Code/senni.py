192aa6934dfc3160b6b73854d410a17bc3bd6534
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-09-13 12:32:04 -0400

    Fix senni.py fake data generation

--- MonteCarloMarginalizeCode/Code/senni.py
+++ MonteCarloMarginalizeCode/Code/senni.py
@@ -91,6 +91,31 @@ class Interpolator(object): # interpolator
                   input_valid[:, dim], _, _ = self.preprocessing(input_valid[:, dim], mu=mu, sigma=sigma)
                   input_test[:, dim], _, _ = self.preprocessing(input_test[:, dim], mu=mu, sigma=sigma)
 
+                  # take the max and min values of the array
+                  true_min,true_max = np.min(input_train[:,dim]),np.min(input_train[:,dim])
+                  # find target range
+                  #   - choice depends on if signs are different!
+                  #   - if same sign, use a factor 
+                  test_min, test_max = 0.75*true_min,1.25*true_max
+                  if test_min*test_max < 0: # two different signs for this parameter
+                      test_min, test_max = 1.25*true_min, 1.25*true_max
+                  fakesamples = np.linspace(test_min, test_max, 0.1*input_train.shape[0])
+                  # remove all the values which occur in the dataset (keep np.where(newv1<min) and np.where(newv1>max))
+                  bad_idxs = np.where((fakesamples > (true_min)) & (fakesamples < (true_max)))
+                  fakesamples = np.delete(fakesamples, bad_idxs)
+                  # set random values (draw from normal) for other parameters and then set likelihood = 0
+                  otherdims = np.random.normal(size=(fakesamples.shape[0], self.n_inputs-1))
+                  #print otherdims
+                  # insert the non-random fakesamples into the relevant dimension index
+                  fakesamples = np.insert(otherdims, dim, fakesamples.T, axis=1)
+                  zerolnLs = np.zeros(fakesamples.shape[0])
+                  zerolnLs = zerolnLs[:, np.newaxis]
+                  unitysigmas = np.ones(fakesamples.shape[0])
+                  unitysigmas = unitysigmas[:, np.newaxis]
+                  # input_train = np.append(input_train, extension, axis=0)
+                  input_train = np.append(input_train, fakesamples, axis=0)
+                  target_train = np.append(target_train, zerolnLs, axis=0)
+                  errors_train = np.append(errors_train, unitysigmas, axis=0)
                   min, max = 2*np.min(input_train[:, dim]), 2*np.max(input_train[:, dim])
 
                   fakesamples = np.linspace(min, max, 0.5*input_train.shape[0])
