00e2a3106d9347a92562da48fda6ebb12134a5b4
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-09-16 09:55:47 -0400

    setup.py: making sure all relevant scripts identified

--- setup.py
+++ setup.py
@@ -16,20 +16,35 @@ REQUIREMENTS = {
 
 #print REQUIREMENTS
 
+import glob
+my_scripts = glob.glob("MonteCarloMarginalizeCode/Code/*.py")
+my_scripts += glob.glob("MonteCarloMarginalizeCode/Code/*.sh")
+my_scripts += glob.glob("MonteCarloMarginalizeCode/Code/integrate_likelihood*")
+my_scripts += glob.glob("MonteCarloMarginalizeCode/Code/convert*")
+
+# Remove old things
+#  - ourio.py, ourparams.py
+#  - anything with 'test'
+
+print " Identified scripts ", my_scripts
+
 setuptools.setup(
     name="RIFT",
-    version="0.0.1",
+    version="0.0.3",
     author="Richard O'Shaughnessy",
     author_email="richard.oshaughnessy@ligo.org",
     description="RIFT parameter estimation pipeline. Note branch used is temp-RIT-Tides-port_master-GPUIntegration!",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://git.ligo.org/richard-oshaughnessy/research-projects-RIT",
+    package_dir = {'':'MonteCarloMarginalizeCode/Code'},
+    py_modules = {"mcsampler", "mcsamplerGPU", "mcsamplerEnsemble", "MonteCarloEnsemble", "lalsimutils",'optimized_GPU_tools', 'Q_inner_product', 'SphericalHarmonics_gpu','vectorized_lal_tools','ROMWaveformManager','factored_likelihood','xmlutils', 'priors_utils', 'dag_utils','statutils', 'bounded_kde','multivariate_truncnorm', 'senni',"PrecessingFisherMatrix","EOSManager", "EOBTidalExternalC"},
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    scripts=my_scripts,
    install_requires=REQUIREMENTS["install"]
 )
