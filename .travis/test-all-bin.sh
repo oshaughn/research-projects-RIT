ee3f3d902928f7d8b39c357ddabd553bbf39a372
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-09-30 14:39:04 -0400

    Attempt at CI test from Duncan M; see https://git.ligo.org/sccb/requests/issues/301#note_119609

---
+++ .travis/test-all-bin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Travis CI test runner: test --help for all command-line executables
+# Author: Duncan Macleod <duncan.macleod@ligo.org> (2016)
+
+# loop over all bin/ scripts
+for EXE in bin/*; do
+    # get file-name as PATH executable
+    EXENAME=`basename ${EXE}`
+    EXEPATH=`which ${EXENAME}`
+    # execute --help with coverage
+    echo "Testing $EXEPATH --help..."
+    coverage run --append --source=RIFT ${EXEPATH} --help 1>/dev/null;
+done
