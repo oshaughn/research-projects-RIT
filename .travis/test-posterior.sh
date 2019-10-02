9282c3eb11dd76b32ec851c4a2c5d1c05469ae4e
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-10-02 09:19:30 -0400

    CI:  (a) CIP test performs both samplers; (b) ILE test runtime reduced

--- .travis/test-posterior.sh
+++ .travis/test-posterior.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 python .travis/make_fake_composite.py
-util_ConstructIntrinsicPosterior_GenericCoordinates.py  --fname fake.composite  --parameter mtot --parameter q --parameter s1z --parameter s2z --sampler-method GMM --use-precessing --no-plots
+# Test default sampler
+util_ConstructIntrinsicPosterior_GenericCoordinates.py  --fname fake.composite  --parameter mtot --parameter q --parameter s1z --parameter s2z  --use-precessing --no-plots
+# Test alternative sampler
+util_ConstructIntrinsicPosterior_GenericCoordinates.py  --fname fake.composite  --parameter mtot --parameter q --parameter s1z --parameter s2z  --use-precessing --no-plots --sampler-method GMM
--- .travis/test-run.sh
+++ .travis/test-run.sh
@@ -8,4 +8,6 @@ make test_workflow_batch_gpu_lowlatency
 cd test_workflow_batch_gpu_lowlatency
 # Reduce number of analyses for this worker to 1, to reduce runtime
 switcheroo 'n-events-to-analyze 20' 'n-events-to-analyze 1' command-single.sh
+# Reduce the number of points investigated by x100
+switcheroo 'n-max 2000000' 'n-max 50000' command-single.sh
 ./command-single.sh
