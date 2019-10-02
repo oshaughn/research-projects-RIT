ac15c79cf6a85c752e6653260238fd0a53c38d43
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-10-02 07:52:50 -0400

    CI test-run.sh:  modify to reduce runtime (only one likelihood evaluation)

--- .travis/test-run.sh
+++ .travis/test-run.sh
@@ -6,4 +6,6 @@ git  clone https://github.com/oshaughn/ILE-GPU-Paper.git
 cd ILE-GPU-Paper/demos/
 make test_workflow_batch_gpu_lowlatency
 cd test_workflow_batch_gpu_lowlatency
+# Reduce number of analyses for this worker to 1, to reduce runtime
+switcheroo 'n-events-to-analyze 20' 'n-events-to-analyze 1' command-single.sh
 ./command-single.sh
