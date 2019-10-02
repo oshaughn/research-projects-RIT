7bf3a9ec81116cf8a1e686a8ad54e231648e10c8
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-10-02 09:33:42 -0400

    CI: test-posterior: fix filenames

--- .travis/test-posterior.sh
+++ .travis/test-posterior.sh
@@ -2,6 +2,6 @@
 
 python .travis/make_fake_composite.py
 # Test default sampler
-util_ConstructIntrinsicPosterior_GenericCoordinates.py  --fname fake.composite  --parameter mtot --parameter q --parameter s1z --parameter s2z  --use-precessing --no-plots
+util_ConstructIntrinsicPosterior_GenericCoordinates.py  --fname .travis/fake.composite  --parameter mtot --parameter q --parameter s1z --parameter s2z  --use-precessing --no-plots
 # Test alternative sampler
-util_ConstructIntrinsicPosterior_GenericCoordinates.py  --fname fake.composite  --parameter mtot --parameter q --parameter s1z --parameter s2z  --use-precessing --no-plots --sampler-method GMM
+util_ConstructIntrinsicPosterior_GenericCoordinates.py  --fname .travis/fake.composite  --parameter mtot --parameter q --parameter s1z --parameter s2z  --use-precessing --no-plots --sampler-method GMM
