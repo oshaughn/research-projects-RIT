79186ae7d73d97fcd0fed649dbadcbd04db68f24
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2019-10-07 10:40:02 -0400

    util_FitAndEvaluate_GenericCoordinates: update import. (not used/development code)

--- MonteCarloMarginalizeCode/Code/bin/util_FitAndEvaluate_GenericCoordinates.py
+++ MonteCarloMarginalizeCode/Code/bin/util_FitAndEvaluate_GenericCoordinates.py
@@ -44,7 +44,7 @@ except ImportError:
 
 
 from sklearn.preprocessing import PolynomialFeatures
-import ModifiedScikitFit as msf  # altenative polynomialFeatures
+import RIFT.misc.ModifiedScikitFit as msf  # altenative polynomialFeatures
 from sklearn import linear_model
 
 from glue.ligolw import lsctables, utils, ligolw
