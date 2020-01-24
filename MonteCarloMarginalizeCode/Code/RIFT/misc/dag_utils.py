b7b0d3908b2caca3d3b611c0d0f576a476eb6db6
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2020-01-24 11:01:55 -0500

    continue to fix silly error with 509 proxy copying

--- MonteCarloMarginalizeCode/Code/RIFT/misc/dag_utils.py
+++ MonteCarloMarginalizeCode/Code/RIFT/misc/dag_utils.py
@@ -704,8 +704,8 @@ echo Starting ...
         if 'X509_USER_PROXY' in os.environ.keys():
             print " Storing copy of X509 user proxy -- beware expiration! "
             cwd = os.getcwd()
-            fname_proxy ="cp ${X509_USER_PROXY} "  + cwd +"/my_proxy"  # this can get overwritten, that's fine - just renews, feature not bug
-            os.system("cp ${X509_USER_PROXY} "  + cwd +"/my_proxy")
+            fname_proxy = cwd +"/my_proxy"  # this can get overwritten, that's fine - just renews, feature not bug
+            os.system("cp ${X509_USER_PROXY} "  + fname_proxy)
 #            ile_job.add_condor_cmd('x509userproxy',os.environ['X509_USER_PROXY'])
             ile_job.add_condor_cmd('x509userproxy',fname_proxy)
 
