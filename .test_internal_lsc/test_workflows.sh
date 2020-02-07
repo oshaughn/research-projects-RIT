b579186af88f8105f1784a72086c16ec10a00d8e
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2020-02-07 08:09:17 -0500

    test_workflows.sh: gstlal not available in py3, switch to BW

--- .test_internal_lsc/test_workflows.sh
+++ .test_internal_lsc/test_workflows.sh
@@ -3,7 +3,7 @@
 
 GID_FIDUCIAL=G347305
 APPROX_FIDUCIAL=IMRPhenomD
-EXTRA_ARGS=" --cip-fit-method rf --cip-sampler-method gmm --calibration C01 --choose-data-LI-seglen --fmin 20 --spin-magnitude-prior zprior_aligned "
+EXTRA_ARGS=" --cip-fit-method rf --cip-sampler-method gmm --calibration C01 --choose-data-LI-seglen --fmin 20 --spin-magnitude-prior zprior_aligned --make-bw-psds "
 
 # standard workflow
 util_RIFT_pseudo_pipe.py --approx ${APPROX_FIDUCIAL} --gracedb-id ${GID_FIDUCIAL} ${EXTRA_ARGS} --manual-postfix _StandardTest0
