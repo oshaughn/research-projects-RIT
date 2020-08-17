4a5364a431fa6c68529739d6db32ad0a08ad112a
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2020-08-16 20:29:30 -0400

    util_RIFT_pseudo_pipe.py: remove format conversion /character error from email client for RU's patch

--- MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
+++ MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
@@ -611,7 +611,7 @@ try:
     from runmonitor import store_run as sto
     if opts.use_ini != None: # making an assumption that opts.use_ini corresponds to prod_O3b file structures, and that opts.use_ini == None corresponds to standard setup with opts.gracedb_id passed. Maybe not a robust assumption…
         level = 2
-        event = os.getcwd.split(“/”)[-2].split(“_”)[0]
+        event = os.getcwd.split("/")[-2].split("_")[0]
     else:
         level = 1
         event = opts.gracedb_id
