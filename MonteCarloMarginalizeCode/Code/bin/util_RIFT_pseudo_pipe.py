9242dfbc964e744c895d5c86c58f092eb9b4c194
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2020-12-14 15:16:12 -0500

    util_RIFT_pseudo_pipe: fix API inconsistency with parsing ini and fake-cacke in ini

--- MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
+++ MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
@@ -409,7 +409,7 @@ if opts.use_ini:
     config.read(opts.use_ini)
     if config.has_option("lalinference", "fake-cache"):
         # dictionary, entries are individual lcf files; we just need to concatenate their contents
-        fake_cache_dict = unsafe_config_get("lalinference","fake-cache")
+        fake_cache_dict = unsafe_config_get(config,["lalinference","fake-cache"])
         fake_cache_fnames = [fake_cache_dict[x] for x in fake_cache_dict.keys()]
         cmd_cat = 'cat ' + ' '.join(fake_cache_fnames) + ' > local.cache'
         os.system(cmd_cat)
