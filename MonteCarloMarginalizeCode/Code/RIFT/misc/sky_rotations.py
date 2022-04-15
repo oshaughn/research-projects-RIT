95298e2d7f9d61e06c5f06957774a0836bb9a1a7
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2022-04-14 20:07:20 -0400

    sky_rotations: better protect rotations to localize frames

--- MonteCarloMarginalizeCode/Code/RIFT/misc/sky_rotations.py
+++ MonteCarloMarginalizeCode/Code/RIFT/misc/sky_rotations.py
@@ -14,8 +14,10 @@ import numpy as np
 import lalsimulation as lalsim
 import lal
 import RIFT.lalsimutils as lalsimutils
+import functools
 
 # defaults at start
+# these global variables don't work as well as we would like
 frm=np.diag(np.ones(3))
 frmInverse=np.diag(np.ones(3))
 vecZ = np.array([0,0,1])
@@ -23,6 +25,10 @@ vecZnew = np.array([0,0,1])
 
 
 def assign_sky_frame(det0,det1,theEpochFiducial):
+    global frm
+    global frmInverse
+    global vecZ
+    global vecZnew
     theDetectorSeparation = lalsim.DetectorPrefixToLALDetector(det0).location - lalsim.DetectorPrefixToLALDetector(det1).location
     vecZ = np.array(theDetectorSeparation/np.sqrt( np.dot(theDetectorSeparation,theDetectorSeparation)))
     time_angle =  np.mod( lal.GreenwichMeanSiderealTime(theEpochFiducial), 2*np.pi)
@@ -31,25 +37,25 @@ def assign_sky_frame(det0,det1,theEpochFiducial):
     frmInverse= np.asarray(np.matrix(frm).I)                                    # Create an orthonormal frame to undo the transform above
 
 
-def rotate_sky_forwards_scalar(theta,phi):   # When theta=0 we are describing the coordinats of the zhat direction in the vecZ frame
-    global frm
+def rotate_sky_forwards_scalar(theta,phi,frm=frm):   # When theta=0 we are describing the coordinats of the zhat direction in the vecZ frame
+#    global frm
     return lalsimutils.polar_angles_in_frame_alt(frm, theta,phi)
 
-def rotate_sky_forwards(th,ph):
+def rotate_sky_forwards(th,ph,frm=frm):
     th_out= np.zeros(len(th))
     ph_out= np.zeros(len(th))
     for indx in np.arange(len(th)):
-        th_out[indx],ph_out[indx] = rotate_sky_forwards_scalar(th[indx],ph[indx])
+        th_out[indx],ph_out[indx] = rotate_sky_forwards_scalar(th[indx],ph[indx],frm=frm)
         return th_out, ph_out
     
 
-def rotate_sky_backwards_scalar(theta,phi): # When theta=0, the vector should be along the vecZ direction and the polar angles returned its polar angles
-    global frmInverse
+def rotate_sky_backwards_scalar(theta,phi,frmInverse=frmInverse): # When theta=0, the vector should be along the vecZ direction and the polar angles returned its polar angles
+#    global frmInverse
     return lalsimutils.polar_angles_in_frame_alt(frmInverse, theta,phi)
 
-def rotate_sky_backwards(th,ph):
+def rotate_sky_backwards(th,ph,frmInverse=frmInverse):
     th_out= np.zeros(len(th))
     ph_out= np.zeros(len(th))
     for indx in np.arange(len(th)):
-            th_out[indx],ph_out[indx] = rotate_sky_backwards_scalar(th[indx],ph[indx])
+            th_out[indx],ph_out[indx] = rotate_sky_backwards_scalar(th[indx],ph[indx],frmInverse=frmInverse)
     return th_out, ph_out
