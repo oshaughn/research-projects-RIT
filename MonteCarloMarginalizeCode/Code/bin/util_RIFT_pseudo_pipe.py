b002ba506803c6e84c50e30f41fa2e53fd5271e1
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2022-04-25 16:27:35 -0400

    pseudo_pipe: if fake data, pass IFO list from coinc

--- MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
+++ MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
@@ -588,7 +588,7 @@ if opts.use_ini:
         fake_cache_fnames = [fake_cache_dict[x] for x in fake_cache_dict.keys()]
         cmd_cat = 'cat ' + ' '.join(fake_cache_fnames) + ' > local.cache'
         os.system(cmd_cat)
-        cmd += " --cache local.cache --fake-data "
+        cmd += " --cache local.cache --fake-data --manual-ifo-list [{}] ".format(event_dict['IFOs'])
 if opts.fake_data_cache:
     cmd += " --cache {} --fake-data ".format(opts.fake_data_cache)
 print( cmd)
