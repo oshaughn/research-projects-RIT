8b959813dc0e79cffd84e709112cda7e3aa44ced
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2022-04-25 16:34:47 -0400

    pseudo_pipe: put ifo argument in the right place

--- MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
+++ MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
@@ -590,7 +590,9 @@ if opts.use_ini:
         os.system(cmd_cat)
         cmd += " --cache local.cache --fake-data  "
 if opts.fake_data_cache:
-    cmd += " --cache {} --fake-data  --manual-ifo-list [{}] ".format(opts.fake_data_cache,event_dict['IFOs'])
+    cmd += " --cache {} --fake-data  "
+    if len(event_dict["IFOs"]) >0 :
+        cmd += " --manual-ifo-list {} ".format(opts.fake_data_cache,event_dict['IFOs'])
 print( cmd)
 os.system(cmd)
 #sys.exit(0)
