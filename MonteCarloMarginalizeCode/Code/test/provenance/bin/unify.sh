3b83e4d8b5de255be622433ee7293d2f3c2d513a
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2022-05-02 10:23:10 -0400

    provenance: better tracking

--- MonteCarloMarginalizeCode/Code/test/provenance/bin/unify.sh
+++ MonteCarloMarginalizeCode/Code/test/provenance/bin/unify.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-echo Input:  /home/richard.oshaughnessy/RIFT_develUWM/src/research-projects-RIT/.travis/fulltest_aligned5/G329473_C01_IMRPhenomD_fmin20_fmin-template20_lmax2_default_manualBWpsd_LIseglen8_GMM_rf/*.composite  
+echo Input: *.composite
 echo Output: all.net
---
+++ MonteCarloMarginalizeCode/Code/test/provenance/bin/util_CleanILE.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# util_CleanILE.py
+#
+#  Reads FILE (not stdin). Consolidates ILE entries for the same physical system.
+#  Compare to: util_MassGriCoalesce.py
+
+
+import sys
+import os
+import RIFT.misc.xmlutils as xmlutils
+#from optparse import OptionParser
+from ligo.lw import lsctables, table, utils
+
+import numpy as np
+import RIFT.misc.weight_simulations as weight_simulations
+
+import fileinput
+#import StringIO
+
+data_at_intrinsic = {}
+
+my_digits=5  # safety for high-SNR BNS
+
+tides_on = False
+distance_on = False  
+col_intrinsic = 9
+
+import argparse
+parser = argparse.ArgumentParser(usage="util_CleanILE.py fname1.dat fname2.dat ... ")
+parser.add_argument("fname",action='append',nargs='+')
+parser.add_argument("--eccentricity", action="store_true")
+opts = parser.parse_args()
+
+
+print(" Input: ", ' '.join(opts.fname))
+print(" Output: stdout")
