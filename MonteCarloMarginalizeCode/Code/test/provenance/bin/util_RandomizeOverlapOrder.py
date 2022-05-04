bda3f3556f023c39bf2b1b80f5efa5b624254068
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2022-05-04 07:39:54 -0400

    provenance more bugs output

--- MonteCarloMarginalizeCode/Code/test/provenance/bin/util_RandomizeOverlapOrder.py
+++ MonteCarloMarginalizeCode/Code/test/provenance/bin/util_RandomizeOverlapOrder.py
@@ -25,4 +25,4 @@ print(" Output: ", opts.output_file)
 
 import os
 #os.system(" touch {} ".format(opts.output_file))
-os.system(' echo {}  > {}', ' '.format(' '.join(args), opts.output_file))
+os.system(' echo {}  > {}'.format(' '.join(args), opts.output_file))
