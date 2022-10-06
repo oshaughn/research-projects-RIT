ca07f9084631024d5865430c55ed94bd370f4da3
kjtwagner <47577724+kjtwagner@users.noreply.github.com>
2022-10-06 12:15:29 -0400

    try to fix build issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,8 @@ master_doc = "index"
 
 extensions = ["sphinx.ext.autodoc",
               "sphinx_rtd_theme",
-              "sphinx_tabs.tabs"
+              "sphinx_tabs.tabs",
+              "sphinx_multiversion"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +45,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "requirements.txt"]
 
 
 # -- Options for HTML output -------------------------------------------------
