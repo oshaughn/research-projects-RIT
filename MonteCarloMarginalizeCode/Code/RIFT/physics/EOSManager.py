de6421734cb7a9594ed89baf8d495430b6fc708d
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2022-12-07 14:36:52 -0500

    EOSManager: typo fix from Atul K.

--- MonteCarloMarginalizeCode/Code/RIFT/physics/EOSManager.py
+++ MonteCarloMarginalizeCode/Code/RIFT/physics/EOSManager.py
@@ -83,7 +83,7 @@ class EOSConcrete:
         Estimate m_b = m_g + m_g^2/(R_{1.4}/km) based on https://arxiv.org/pdf/1905.03784.pdf Eq. (6)
         """
         r1p4 =lalsim.SimNeutronStarRadius(1.4*lal.MSUN_SI, self.eos_fam)/1e3
-        return m + (1./r1p4)*m*(m/lal.MSUN_SI)
+        return m + (1./r1p4)*m**2 #(m/lal.MSUN_SI)
 
     def pressure_density_on_grid_alternate(self,logrho_grid,enforce_causal=False):
         """ 
