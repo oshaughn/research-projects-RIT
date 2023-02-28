7ceb767a5ec3a5f24d835a095e6ec7f8bcec0e33
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2023-02-28 11:52:02 -0500

    factored_likelihood: add gwsignal fork

--- MonteCarloMarginalizeCode/Code/RIFT/likelihood/factored_likelihood.py
+++ MonteCarloMarginalizeCode/Code/RIFT/likelihood/factored_likelihood.py
@@ -71,6 +71,12 @@ if not( 'RIFT_LOWLATENCY'  in os.environ):
         useNR=False
 
  try:
+        import RIFT.physics.GWSignal as rgws
+        has_GWS=True
+ except:
+        has_GWS=False
+
+ try:
         import RIFT.physics.ROMWaveformManager as romwf
         print(" factored_likelihood.py: ROMWaveformManager as romwf")
         useROM=True
@@ -109,6 +115,7 @@ def PrecomputeLikelihoodTerms(event_time_geo, t_window, P, data_dict,
         inv_spec_trunc_Q=False, T_spec=0., verbose=True,quiet=False,
          NR_group=None,NR_param=None,
         ignore_threshold=1e-4,   # dangerous for peak lnL of 25^2/2~300 : biases
+        use_gwsignal=False,
        use_external_EOB=False,nr_lookup=False,nr_lookup_valid_groups=None,no_memory=True,perturbative_extraction=False,perturbative_extraction_full=False,hybrid_use=False,hybrid_method='taper_add',use_provided_strain=False,ROM_group=None,ROM_param=None,ROM_use_basis=False,ROM_limit_basis_size=None,skip_interpolation=False):
     """
     Compute < h_lm(t) | d > and < h_lm | h_l'm' >
@@ -219,7 +226,7 @@ def PrecomputeLikelihoodTerms(event_time_geo, t_window, P, data_dict,
                         print(" FFT for conjugate mode ", mode, hlmsT[mode].data.length)
                 hlmsT[mode].data.data = np.conj(hlmsT[mode].data.data)
                 hlms_conj[mode] = lsu.DataFourier(hlmsT[mode])
-    elif (not (NR_group) or not (NR_param)) and  (not use_external_EOB) and (not nr_lookup):
+    elif (not (NR_group) or not (NR_param)) and  (not use_external_EOB) and (not nr_lookup) and (not use_gwsignal):
         if not quiet:
                 print( "  FACTORED LIKELIHOOD WITH hlmoff (default ChooseTDModes) " )
         # hlms_list = lsu.hlmoff(P, Lmax) # a linked list of hlms
@@ -233,6 +240,11 @@ def PrecomputeLikelihoodTerms(event_time_geo, t_window, P, data_dict,
         # else:
         #         hlms_conj = hlms_conj_list
         hlms, hlms_conj = lsu.std_and_conj_hlmoff(P,Lmax)
+    elif use_gwsignal and (has_GWS):
+        if not quiet:
+            print( "  FACTORED LIKELIHOOD WITH hlmoff (GWsignal) " )            
+        hlms, hlms_conj = lsu.std_and_conj_hlmoff(P,Lmax)
+
     elif (nr_lookup or NR_group) and useNR:
 	    # look up simulation
 	    # use nrwf to get hlmf
