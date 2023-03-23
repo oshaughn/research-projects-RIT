b0a97e9466042e5a278766479549544683034310
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2023-03-23 15:02:20 -0400

    lalsimutils: add extra extract option, to enable plots of 1/q easily

--- MonteCarloMarginalizeCode/Code/RIFT/lalsimutils.py
+++ MonteCarloMarginalizeCode/Code/RIFT/lalsimutils.py
@@ -1263,6 +1263,14 @@ class ChooseWaveformParams:
             terms = a.split(',')
             vals = map(self.extract_param, terms) # recurse to parse lower-level quantities
             return np.prod(vals)
+        if 'inverse(' in p:
+            # Drop first and last characters
+            a=p.replace(' ', '') # drop spaces
+            a = a[:len(a)-1] # drop last
+            a = a[8:]
+            terms = a.split(',')
+            vals = map(self.extract_param, terms) # recurse to parse lower-level quantities
+            return np.prod(vals)
         # assign an attribute
         if hasattr(self,p):
             return getattr(self,p)
