44ed2b2295c3343ceceb0a7610db9f0ccbea6883
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2023-04-17 14:36:05 -0400

    convert_ascii_framechange.py: workaround for XPHM frame choice

---
+++ MonteCarloMarginalizeCode/Code/bin/convert_ascii_framechange_xphm.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+#
+#  Usage
+#     Workaround for an analysis being done in J frame unintentionally.
+#     Note fref is MANDATORY for such a conversion
+
+import numpy as np
+
+import lalsimulation as lalsim
+import lalsimutils
+import numpy.lib.recfunctions as rfn
+
+
+optp = OptionParser()
+optp.add_option("--extrinsic-posterior-file",default=None,type=str,help="Input file")
+optp.add_option("--fref",default=20,type=float,help="Reference frequency. Depending on approximant and age of implementation, may be ignored")
+opts, args = optp.parse_args()
+
+
+
+# 
+dat = np.genfromtxt(opts.extrinsic_posterior_file, names=True)
+npts = len(dat['m1'])
+dat_out =np.recarray(dat)
+
+
+for indx in np.arange(npts):
+    # Should use the vectorized convert tool, but not immplemented for this yet
+    P =lalsimutils.ChooseWaveformParams()
+    # Intrinsic parameters
+    P.assign_param('m1', dat['m1'][indx])
+    P.assign_param('m2', dat['m2'][indx])
+    P.assign_param('s1x', dat['a1x'][indx])
+    P.assign_param('s1y', dat['a1y'][indx])
+    P.assign_param('s1z', dat['a1z'][indx])
+    P.assign_param('s2x', dat['a2x'][indx])
+    P.assign_param('s2y', dat['a2y'][indx])
+    P.assign_param('s2z', dat['a2z'][indx])
+    P.assign_param('fref', opts.fref)
+    # stable extrinsic parameters that we *need* to assign
+    P.assign_param('psiJ', dat['psi'][indx])
+    P.assign_param('thetaJN', dat['iota'][indx])
+    P.assign_param('phiJL',dat['phiorb'][indx])
+    dat_out['psi'][indx] = P.psi
+    dat_out['iota'][indx] = P.incl
+    dat_out['phiorb'][indx] = P.phiref
+
+np.savetxt(dat_out, opts.fname_out,header='# ' + ' '.join(dat_out.dtype.names))
