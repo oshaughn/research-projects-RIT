77fc7e7372c93da2a5192a6c4cd2d494a463a7f1
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2023-07-17 08:57:56 -0400

    __init__: declare plot_utilities submodule

--- MonteCarloMarginalizeCode/Code/RIFT/__init__.py
+++ MonteCarloMarginalizeCode/Code/RIFT/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ['integrators','interpolators','likelihood','physics','misc','calmarg']
+__all__ = ['integrators','interpolators','likelihood','physics','misc','calmarg','plot_utilities']
 from . import lalsimutils  # top-level interface utility
 from .  import *   # sub-packages
