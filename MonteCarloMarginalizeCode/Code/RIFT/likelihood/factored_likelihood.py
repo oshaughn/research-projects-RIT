61f1c4e2a6ed3cf70b65302c9bbb399eb6d8e03e
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2024-06-17 10:35:34 -0400

    factored_likelihood: call with tapering (note lalsimutils uses tapering as default ) to be consistent

--- MonteCarloMarginalizeCode/Code/RIFT/likelihood/factored_likelihood.py
+++ MonteCarloMarginalizeCode/Code/RIFT/likelihood/factored_likelihood.py
@@ -164,6 +164,7 @@ def internal_hlm_generator(P,
     elif use_gwsignal and (has_GWS):  # this MUST be called first, so the P.approx is never tested
         if not quiet:
             print( "  FACTORED LIKELIHOOD WITH hlmoff (GWsignal) " )            
+        P.taper =  lsu.lsu_TAPER_START # MUST DO
         hlms, hlms_conj = rgws.std_and_conj_hlmoff(P,Lmax,approx_string=use_gwsignal_approx,**extra_waveform_kwargs)
 
     elif (not nr_lookup) and (not NR_group) and ( P.approx ==lalsim.SEOBNRv2 or P.approx == lalsim.SEOBNRv1 or P.approx==lalsim.SEOBNRv3 or P.approx == lsu.lalSEOBv4 or P.approx ==lsu.lalSEOBNRv4HM or P.approx == lalsim.EOBNRv2 or P.approx == lsu.lalTEOBv2 or P.approx==lsu.lalTEOBv4 ):
