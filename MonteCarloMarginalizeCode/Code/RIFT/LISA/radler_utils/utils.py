597ac26505d3f1ef4ad5d14143551b770d1ea788
Aasim Z Jan <58118417+AasimZahoor@users.noreply.github.com>
2024-09-08 12:29:21 -0500

    utils.py: removed a print statement

--- MonteCarloMarginalizeCode/Code/RIFT/LISA/radler_utils/utils.py
+++ MonteCarloMarginalizeCode/Code/RIFT/LISA/radler_utils/utils.py
@@ -2,15 +2,14 @@ import matplotlib.pyplot as plt
 import lal
 import numpy as np
 import h5py
-
 import sys
+from scipy.interpolate import interp1d
+
 RIFT = "RIFT-LISA-3G-O4c"
 sys.path.append(f"/Users/aasim/Desktop/Research/Mcodes/{RIFT}/MonteCarloMarginalizeCode/Code")
 
 import RIFT.lalsimutils as lsu
 from RIFT.LISA.response.LISA_response import *
-print(lsu.__file__)
-from scipy.interpolate import interp1d
 
 ###########################################################################################
 # Functions
