abe0781c458fac5a08bc139fc12768f36b84b51c
Aasim Z Jan <58118417+AasimZahoor@users.noreply.github.com>
2024-09-07 21:32:47 -0500

    radler_utils: first commit to radler utils

---
+++ MonteCarloMarginalizeCode/Code/RIFT/LISA/radler_utils/utils.py
@@ -0,0 +1,131 @@
+import matplotlib.pyplot as plt
+import lal
+import numpy as np
+import h5py
+
+import sys
+RIFT = "RIFT-LISA-3G-O4c"
+sys.path.append(f"/Users/aasim/Desktop/Research/Mcodes/{RIFT}/MonteCarloMarginalizeCode/Code")
+
+import RIFT.lalsimutils as lsu
+from RIFT.LISA.response.LISA_response import *
+print(lsu.__file__)
+from scipy.interpolate import interp1d
+
+###########################################################################################
+# Functions
+###########################################################################################
+def create_resampled_lal_COMPLEX16TimeSeries(tvals, new_tvals, data_dict):
+    data_dict_new = {}
+    new_deltaT = np.diff(new_tvals)[0]
+    for channel in data_dict.keys():
+        print(f"Reading channel {channel}")
+        func = interp1d(tvals, data_dict[channel], fill_value=tuple([0,0]), bounds_error=False)
+        new_data = func(new_tvals)
+
+        ht_lal = lal.CreateCOMPLEX16TimeSeries("ht_lal", 0.0, 0, new_deltaT, lal.DimensionlessUnit, len(new_data))    
+        ht_lal.data.data = new_data + 0j
+        print(f" Delta T = {ht_lal.deltaT} s, size = {ht_lal.data.length}, time = {ht_lal.data.length*ht_lal.deltaT/3600/24:2f} days") 
+        data_dict_new[channel] = ht_lal
+    return data_dict_new
+
+
+def get_ldc_psds(save_path=None, fvals=None, channels = ["A", "E", "T"], model = "SciRDv1"):
+    import ldc.lisa.noise as noise
+    if fvals is None:
+        fmin = 0.0
+        deltaT = 8
+        fNyq = 0.5/deltaT
+        deltaF = 1/(4194304*deltaT)
+        fvals = np.arange(fmin, fNyq, deltaF)
+        print(f"Generating psds using model = {model}, fmin = {fmin}Hz, fmax = {fNyq}Hz, 1/deltaF = {4194304*deltaT}s, deltaT = {deltaT}s.")
+    noise_model = noise.get_noise_model(model, fvals)
+    Sn = {}
+    Sn["fvals"] = fvals
+    for channel in channels:
+        print(f"Channel = {channel}")
+        Sn[channel] = noise_model.psd(fvals, channel)
+        if save_path:
+             np.savetxt(f"{save_path}/{channel}_psd.txt", np.vstack([Sn["fvals"], Sn[channel]]).T)       
+    return Sn
+
+def generate_data_from_radler(h5_path, output_as_AET = False, new_tvals =  None, output_as_FD=False):
+    """This function takes in a radler h5 file and outputs a data dictionary.
+        Args:
+            h5_path (string)       : path to radler h5 file,
+            output_as_AET (boolean): set to True if you want the data as A, E, T,
+            new_tvals (numpy array): pass the new tvals to truncate and/or resample the data,
+            output_as_FD (boolean) : set to True if you want the data in frequency domain,
+        Returns:
+            data_dictionary"""
+    # Load data
+    data = h5py.File(h5_path)
+    # Extract X, Y, Z data
+    XYZ_data = np.array(data["H5LISA"]['PreProcess']['TDIdata'])
+    # DeltaT
+    cadence  = np.array(data["H5LISA"]["GWSources"]['MBHB-0']['Cadence'])
+    # Save as dictionary
+    data_dict = {}
+    data_dict["X"], data_dict["Y"], data_dict["Z"] = XYZ_data[:,1], XYZ_data[:,2], XYZ_data[:,3]
+    # tvals for this data
+    old_tvals = XYZ_data[:,0]
+
+    # Convert into AET if requested
+    if output_as_AET:
+        tmp_dict = data_dict
+        data_dict = {}
+        data_dict["A"] = 1/np.sqrt(2) * (tmp_dict["Z"]- tmp_dict["X"])
+        data_dict["E"] = 1/np.sqrt(6) * (tmp_dict["X"] - 2*tmp_dict["Y"] + tmp_dict["Z"])
+        data_dict["T"] = 1/np.sqrt(3) * (tmp_dict["X"]+ tmp_dict["Y"] + tmp_dict["Z"])
+    # if new_tvals is not provided, use old ones so no interpolation but still convert into COMPLEX16TimeSeries
+    if new_tvals is None:
+        new_tvals = old_tvals
+    data_dict = create_resampled_lal_COMPLEX16TimeSeries(old_tvals, new_tvals, data_dict)
+    # Convert into FD if requested
+    if output_as_FD:
+        power = np.log2(len(new_tvals))
+        assert power == np.ceil(power), "The data bins need to be power of 2 for lal FFT routines, make sure len(new_tvals) is a power of 2."
+        tmp_dict = data_dict
+        data_dict = {}
+        for channel in tmp_dict:
+            data_dict[channel] = lsu.DataFourier(tmp_dict[channel])
+    return data_dict
+
+
+def get_radler_mbhb_params(h5_path):
+    """This function takes in a radler h5 file and outputs the parameter of the MBHB injection as a dictionary.
+        Args:
+            h5_path (string)       : path to radler h5 file,
+        Returns:
+            parameter dictionary"""
+    # Load data
+    data = h5py.File(h5_path)
+    # Extract params
+    pGW = data["H5LISA"]["GWSources"]['MBHB-0']
+    params = {}
+    params["m1"] = np.array(pGW.get('Mass1'))
+    params["m2"] = np.array(pGW.get('Mass2'))
+    params["chi1"] = np.array(pGW.get('Spin1')*np.cos(pGW.get('PolarAngleOfSpin1')))
+    params["chi2"] = np.array(pGW.get('Spin2')*np.cos(pGW.get('PolarAngleOfSpin2')))
+
+    theL = np.array(pGW.get('InitialPolarAngleL'))
+    phiL = np.array(pGW.get('InitialAzimuthalAngleL'))
+    longt = np.array(pGW.get('EclipticLongitude'))
+    lat = np.array(pGW.get('EclipticLatitude'))
+
+    params["tc"] = np.array(pGW.get('CoalescenceTime'))
+    params["phi0"] = np.array(pGW.get('PhaseAtCoalescence'))
+    params["DL"]  = np.array(pGW.get('Distance'))
+
+    dist  = params["DL"]  * 1.e6 * lal.PC_SI
+    # print ("DL = ", DL*1.e-3, "Gpc")
+    params["beta"] = np.array(lat)
+    params["lambda"] = np.array(longt)
+    params["incl"] = np.array(np.arccos( np.cos(theL)*np.sin(lat) + np.cos(lat)*np.sin(theL)*np.cos(longt-phiL)))
+
+    up_psi = np.array(np.sin(params["beta"])*np.sin(theL)*np.cos(params["lambda"] - phiL) - np.cos(theL)*np.cos(params["beta"]))
+    down_psi = np.array(np.sin(theL)*np.sin(params["lambda"] - phiL))
+    params["psi"] = np.array(np.arctan2(up_psi, down_psi))
+    params["z"] = np.array(pGW.get("Redshift"))
+
+    return params
