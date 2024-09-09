e1390ab69ff147d8e5d429cd4096e0e9dd5c39f8
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2024-09-09 14:36:06 -0400

    ROMWaveformCatalogManager: disable print statements about imports by default (RIFT_LOUD variable)

--- MonteCarloMarginalizeCode/Code/RIFT/physics/ROMWaveformManager.py
+++ MonteCarloMarginalizeCode/Code/RIFT/physics/ROMWaveformManager.py
@@ -3,30 +3,36 @@
 debug_output = False
 rosDebug = False
 
+from .. import lalsimutils
+
 
 import numpy as np
 import os, sys
 try:
     import gwtools
     import gwsurrogate as gws
-    print("  gwsurrogate: ",  gws.__file__)
-    print("  gwtools: ",  gwtools.__file__)
+    if lalsimutils.log_loud:
+        print("  gwsurrogate: ",  gws.__file__)
+        print("  gwtools: ",  gwtools.__file__)
 except:
-    print(" - no gwsurrogate - (almost everything from ROMWaveformManager will hard fail if you use it) ")
+    if lalsimutils.log_loud:
+        print(" - no gwsurrogate - (almost everything from ROMWaveformManager will hard fail if you use it) ")
 try:
     import NRSur7dq2
-    print("  NRSur7dq2: ", NRSur7dq2.__version__, NRSur7dq2.__file__)
+    if lalsimutils.log_loud:
+        print("  NRSur7dq2: ", NRSur7dq2.__version__, NRSur7dq2.__file__)
 except:
-    print(" - no NRSur7dq2 - ")
+    if lalsimutils.log_loud:
+        print(" - no NRSur7dq2 - ")
 
 import lalsimulation as lalsim
 import lal
 
-from .. import lalsimutils
 try:
     import LALHybrid
 except:
-    print(" - no hybridization - ")
+    if lalsimutils.log_loud:
+        print(" - no hybridization - ")
 
 from scipy.interpolate import interp1d
 from scipy.linalg import inv
@@ -38,10 +44,12 @@ import h5py
 try:
     dirBaseFiles =os.environ["GW_SURROGATE"] # surrogate base directory
 except:
-    print( " ==> WARNING:  GW_SURROGATE environment variable is not set <== ")
-    print( "    Only surrogates with direct implementation are available (NRSur7dq2) ")
+    if lalsimutils.log_loud:
+        print( " ==> WARNING:  GW_SURROGATE environment variable is not set <== ")
+        print( "    Only surrogates with direct implementation are available (NRSur7dq2) ")
 
-print(" ROMWaveformManager: ILE version")
+if lalsimutils.log_loud:     
+    print(" ROMWaveformManager: ILE version")
 
 #default_interpolation_kind = 'quadratic'  # spline interpolation   # very slow! 
 default_interpolation_kind = 'linear'  # robust, fast
