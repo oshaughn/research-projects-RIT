2a77810818adcd520eb0cea96e2fde3a44b7ad21
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2024-12-12 11:15:39 -0500

    test-run.sh: struggling with making pip actually run, very strange behavior unlike normal

--- .travis/test-run.sh
+++ .travis/test-run.sh
@@ -5,7 +5,8 @@
 #if  command -v apt ; then
   #apt install lalsuite=7.22   # problem with 7.23 and newer lalapps_path2cache, workaround
 #fi
-pip install lalsuite==7.22 --break-system-packges
+#pip install lalsuite==7.22 --break-system-packges
+python -m pip install --upgrade lalsuite==7.22 --break-system-packages
 
 git  clone https://github.com/oshaughn/ILE-GPU-Paper.git
 cd ILE-GPU-Paper/demos/
