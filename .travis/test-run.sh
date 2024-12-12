ae37d6494c8a7419040a166fae86308f50310efa
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2024-12-12 10:53:16 -0500

    test-run.sh: correct syntax for apt install

--- .travis/test-run.sh
+++ .travis/test-run.sh
@@ -3,7 +3,7 @@
 # GETTING_STARTED.md example
 
 if  command -v apt ; then
-  apt install lalsuite==7.22   # problem with 7.23 and newer lalapps_path2cache, workaround
+  apt install lalsuite=7.22   # problem with 7.23 and newer lalapps_path2cache, workaround
 fi 
 
 git  clone https://github.com/oshaughn/ILE-GPU-Paper.git
