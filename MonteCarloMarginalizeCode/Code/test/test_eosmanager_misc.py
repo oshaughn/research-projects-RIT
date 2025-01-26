0c4713928584adccbba9aede3ec6d3c838bf61c2
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2025-01-26 13:12:25 -0500

    test_eosmanager_misc.py: want to have use case example for EOSManager in test, for easier reference for students

---
+++ MonteCarloMarginalizeCode/Code/test/test_eosmanager_misc.py
@@ -0,0 +1,53 @@
+
+
+
+import numpy as np
+import matplotlib
+import os
+import matplotlib.pyplot as plt
+import argparse
+
+
+import lal
+import RIFT.lalsimutils as lalsimutils
+import RIFT.physics.EOSManager as EOSManager
+import lalsimulation as lalsim
+from scipy.integrate import nquad
+from RIFT.plot_utilities.EOSPlotUtilities import render_eos
+
+DENSITY_CGS_IN_MSQUARED=1000*lal.G_SI/lal.C_SI**2  # g/cm^3 -> 1/m^2 //GRUnits. Multiply by this to convert from CGS -> 1/m^2 units (_geom). lal.G_SI/lal.C_SI**2 takes kg/m^3 -> 1/m^2  ||  https://lscsoft.docs.ligo.org/lalsuite/lalsimulation/_l_a_l_sim_neutron_star_8h_source.html
+PRESSURE_CGS_IN_MSQUARED = DENSITY_CGS_IN_MSQUARED/(lal.C_SI*100)**2
+
+matplotlib.rcParams.update({'font.size': 12.0,  'mathtext.fontset': 'stix'})
+matplotlib.rcParams['figure.figsize'] = (9.0, 7.0)
+matplotlib.rcParams['xtick.labelsize'] = 15.0
+matplotlib.rcParams['ytick.labelsize'] = 15.0
+matplotlib.rcParams['axes.labelsize'] = 25.0
+matplotlib.rcParams['lines.linewidth'] = 2.0
+plt.style.use('seaborn-v0_8-whitegrid')
+
+
+# File location
+eos_base_dir = os.environ['LALSIMULATION_DATADIR']
+
+
+# Demo 1: Tabular i/o
+#    - key point 1: does not fill in data at lower density: finite range
+#    - key point 2: data format follows lalsuite standard, in geometric units 
+eos_names =  ['LALSimNeutronStarEOS_AP4.dat',
+              'LALSimNeutronStarEOS_WFF1.dat']
+for name in eos_names:
+    data = np.loadtxt(eos_base_dir+"/"+name, delimiter = "\t")
+    neweos = EOSManager.EOSFromTabularData(eos_data=data, reject_phase_transitions=True)
+    plt.loglog(data[:,1]/DENSITY_CGS_IN_MSQUARED,data[:,0]/PRESSURE_CGS_IN_MSQUARED, label = name+' raw', ls = 'dashed')
+    plot_render = render_eos(eos=neweos.eos, xvar='energy_density', yvar='pressure')
+    plt.legend()
+    plt.title("EOSFromTabularData comparison to raw data")
+plt.savefig("fig_demo_EOSFromTabular.pdf"); plt.clf()
+
+# Demo 2: Conventional EOS names with lalsuite
+#   - key point: really also using the same low-level interface as tabular data
+#   - key point: LALSimulation interface does NOT paste on low density EOS model
+my_eos = EOSManager.EOSLALSimulation('SLy')
+render_eos(my_eos.eos,'energy_density', 'pressure')
+plt.savefig('fig_demo_EOSLALSimulation.pdf')
