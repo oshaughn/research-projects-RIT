83144768eb92deeb147a781774f8b7561ea9a0fe
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2025-11-03 17:12:04 -0500

    pseudo_pipe: modify so default forces --internal-waveform-taper SIM_INSPIRAL_TAPER_START

--- MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
+++ MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
@@ -190,6 +190,7 @@ parser.add_argument("--internal-ile-n-max",default=None,type=int,help="Set maxim
 parser.add_argument("--internal-ile-inv-spec-trunc-time",default=None,type=float,help="Timescale of inverse spectrum truncation time. Default in pipeline is zero. Should be no more than 1/2 the segment length")
 parser.add_argument("--internal-ile-data-tukey-window-time",default=None,type=float,help="Timescale of the tukey window (total, both sides)")
 parser.add_argument("--internal-ile-psd-common-window",action='store_true',help="Default is to use the window shape correction on the input PSD (assumed to be scaled), and NOT to try to scale PSD.  Adding this option means we assume the PSD is not being window-corrected on input, so does not need rescaling. ")
+parser.add_argument("--internal-ile-modify-taper",default=None,help="String provided modifies taper. If not provided TAPER_START will be used for all waveforms. Future-protecting for nonstandard tapering")
 parser.add_argument("--internal-marginalize-distance",action='store_true',help="If present, the code will marginalize over the distance variable. Passed diretly to helper script. Default will be to generate d_marg script *on the fly*")
 parser.add_argument("--internal-marginalize-distance-file",help="Filename for marginalization file.  You MUST make sure the max distance is set correctly")
 parser.add_argument("--internal-distance-max",type=float,help="If present, the code will use this as the upper limit on distance (overriding the distance maximum in the ini file, or any other setting). *required* to use internal-marginalize-distance in most circumstances")
@@ -952,6 +953,8 @@ if opts.internal_mitigate_fd_J_frame == 'L_frame':
     line += " --internal-waveform-fd-L-frame "
 if opts.internal_ile_inv_spec_trunc_time:
     line = line.replace("inv-spec-trunc-time 0 ","inv-spec-trunc-time {} ".format(opts.internal_ile_inv_spec_trunc_time))
+if not(opts.internal_ile_modify_taper):
+    line += " --internal-waveform-taper SIM_INSPIRAL_TAPER_START " # taper start of waveform by default, overrides any settings in the grids
 with open('args_ile.txt','w') as f:
         f.write(line)
 
