eac68956f497e11e62d35c4de90d67347d92554e
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2026-03-13 10:18:02 -0400

    helper: fix issue with extracting DATAFIND from environment which caused fallback to default

--- MonteCarloMarginalizeCode/Code/bin/helper_LDG_Events.py
+++ MonteCarloMarginalizeCode/Code/bin/helper_LDG_Events.py
@@ -94,6 +94,8 @@ def ldg_datafind(ifo_base, types, server, data_start,data_end,datafind_exe='gw_d
     fname_out = ifo_base[0]+"_local.cache"
     print([ifo_base, types, server, data_start, data_end])
     cmd = datafind_exe + ' -u file --gaps -o ' + ifo_base[0] + ' -t ' + types + ' --server ' + server + ' -s ' + str(data_start) + ' -e ' + str(data_end) + " > " +fname_out_raw
+    print(" Datafind command ")
+    print(cmd)
     os.system(cmd)
 
     if not retrieve:
@@ -424,8 +426,15 @@ if opts.verbose:
 
 datafind_server = None
 try:
-   datafind_server = os.environ['LIGO_DATAFIND_SERVER']
-   print(" LIGO_DATAFIND_SERVER ", datafind_server)
+   if 'GWDATAFIND_SERVER' in os.environ:
+       datafind_server = os.environ['GWDATAFIND_SERVER']
+       print(" GWDATAFIND_SERVER ", datafind_server)
+   elif 'LIGO_DATAFIND_SERVER' in os.environ:
+       datafind_server = os.environ['LIGO_DATAFIND_SERVER']
+       print(" LIGO_DATAFIND_SERVER ", datafind_server)
+   else:
+       print(" No LIGO_DATAFIND_SERVER, using backup ")
+       datafind_server = "datafind.igwn.org:80"
 except:
   print(" No LIGO_DATAFIND_SERVER ")
   datafind_server = "datafind.ligo.org:443"
