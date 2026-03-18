227cb51bb2c3afb3e1134310e9fbb69f8a4f5eb0
Richard O'Shaughnessy <richard.oshaughnessy@ligo.org>
2026-03-18 17:29:40 -0400

    dag_utils.py: allow matching to local pool for ILE jobs if running on OSG

--- MonteCarloMarginalizeCode/Code/RIFT/misc/dag_utils.py
+++ MonteCarloMarginalizeCode/Code/RIFT/misc/dag_utils.py
@@ -1225,7 +1225,8 @@ def write_consolidate_sub_simple(tag='consolidate', exe=None, base=None,target=N
     if no_grid:
         ile_job.add_condor_cmd("MY.DESIRED_SITES",'"nogrid"')
         ile_job.add_condor_cmd("MY.flock_local",'true')
-
+    if use_singularity or use_osg: 
+        ile_job.add_condor_cmd("MY.flock_local",'true')  # jobs can match to local pool !
 
 
     try:
