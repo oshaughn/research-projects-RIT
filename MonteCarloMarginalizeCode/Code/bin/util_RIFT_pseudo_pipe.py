23fa104c43fd6a0b8dd58f7999dfb2d2660f6319
Aasim Z Jan <58118417+AasimZahoor@users.noreply.github.com>
2026-06-18 12:55:43 -0500

    util_RIFT_pseudo_pipe.py: removed type=int that was appearing twice in the force-puff-iterations argument

--- MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
+++ MonteCarloMarginalizeCode/Code/bin/util_RIFT_pseudo_pipe.py
@@ -330,7 +330,7 @@ parser.add_argument("--use-osg-public",action='store_true',help="Activate public
 parser.add_argument("--archive-pesummary-label",default=None,help="If provided, creates a 'pesummary' directory and fills it with this run's final output at the end of the run")
 parser.add_argument("--archive-pesummary-event-label",default="this_event",help="Label to use on the pesummary page itself")
 parser.add_argument("--internal-mitigate-fd-J-frame",default="L_frame",help="L_frame|rotate, choose method to deal with ChooseFDWaveform being in wrong frame. Default is to request L frame for inputs")
-parser.add_argument("--internal-force-puff-iterations", default=4, type=int, type=int, help="Number of iterations to be puffed")
+parser.add_argument("--internal-force-puff-iterations", default=4, type=int, help="Number of iterations to be puffed")
 opts=  parser.parse_args()
 
 config_stored=None; config_dict=None
